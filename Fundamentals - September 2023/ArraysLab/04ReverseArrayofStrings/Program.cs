0be90d33977c390c5643d60a27bb79e600231dc9
Bozhidar1804 <bobo1804@abv.bg>
2023-09-27 23:57:26 +0300

    Solved Ex5 from ArraysLab

---
+++ Fundamentals - September 2023/ArraysLab/04ReverseArrayofStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04ReverseArrayofStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] items = input.Split(" ").ToArray();
+
+            for (int i = items.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{items[i]} ");
+            }
+
+        }
+    }
+}
