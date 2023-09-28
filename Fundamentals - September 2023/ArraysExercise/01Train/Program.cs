d427a67a4cd056773b45240bccbd4fefc6f81996
Bozhidar1804 <bobo1804@abv.bg>
2023-09-28 19:48:40 +0300

    Solved Ex1 from ArraysExercises

---
+++ Fundamentals - September 2023/ArraysExercise/01Train/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] wagons = new int[n];
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                wagons[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int wagon in wagons)
+            {
+                Console.Write($"{wagon} ");
+                sum += wagon;
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
