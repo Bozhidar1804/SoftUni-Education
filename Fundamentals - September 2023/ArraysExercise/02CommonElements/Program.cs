298006d5d729e7eeb6849dc9f66ba56adccc504e
Bozhidar1804 <bobo1804@abv.bg>
2023-09-28 19:59:51 +0300

    Solved Ex2 from ArraysExercise

---
+++ Fundamentals - September 2023/ArraysExercise/02CommonElements/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArr = Console.ReadLine().Split(" ");
+            string[] secondArr = Console.ReadLine().Split(" ");
+
+            string result = "";
+
+            foreach(string s in secondArr)
+            {
+                if(firstArr.Contains(s))
+                {
+                    result += s;
+                    result += " ";
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
