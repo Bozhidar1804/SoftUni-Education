f0834f0860fda6a84bf27fe2f9be58cda8fee183
Bozhidar1804 <bobo1804@abv.bg>
2023-09-30 11:52:41 +0300

    Solved Ex3 from ArraysExercise

---
+++ Fundamentals - September 2023/ArraysExercise/03Zig-ZagArrays/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _03Zig_ZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] firstArr = new int[n];
+            int[] secondArr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] currentNumbers = new int[2];
+                string[] currentInput = Console.ReadLine().Split(" ");
+                currentNumbers[0] = int.Parse(currentInput[0]);
+                currentNumbers[1] = int.Parse(currentInput[1]);
+
+                if (i % 2 != 0)
+                {
+                    firstArr[i] = currentNumbers[0];
+                    secondArr[i] = currentNumbers[1];
+                } else if (i % 2 == 0)
+                {
+                    firstArr[i] = currentNumbers[1];
+                    secondArr[i] = currentNumbers[0];
+                }
+            }
+
+            foreach (int num in secondArr)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.WriteLine();
+
+            foreach (int num in firstArr)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
