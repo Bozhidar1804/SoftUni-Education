4a1d510f43b61db7dadaa787883b7742f82a6f3f
Bozhidar1804 <bobo1804@abv.bg>
2023-10-01 21:58:02 +0300

    Solved Ex7 from ArraysExercises

---
+++ Fundamentals - September 2023/ArraysExercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string sequence = "";
+            string longestSequence = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == input.Length - 1)
+                {
+                    break;
+                }
+
+                if (input[i] == input[i + 1])
+                {
+                    for (int k = i; input[i] == input[k]; k++)
+                    {
+                        sequence += input[k] + " ";
+                        if (sequence.Length > longestSequence.Length)
+                        {
+                            longestSequence = sequence;
+                        }
+
+                        if (k == input.Length - 1)
+                        {
+                            break;
+                        }
+                    }
+                }
+                sequence = "";
+            }
+
+            string[] output = longestSequence.Split();
+            foreach(string s in output)
+            {
+                Console.Write($"{s} ");
+            }
+
+        }
+    }
+}
