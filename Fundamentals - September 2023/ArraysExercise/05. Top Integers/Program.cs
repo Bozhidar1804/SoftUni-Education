178afc95b480438da8c8ff47dfa91e4b95108154
Bozhidar1804 <bobo1804@abv.bg>
2023-10-01 19:26:30 +0300

    Solved Ex5 Top Integers from ArraysExercise

---
+++ Fundamentals - September 2023/ArraysExercise/05. Top Integers/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace _05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] topIntegers = new int[input.Length];
+            bool a = true;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == input.Length - 1)
+                {
+                    topIntegers[i] = input[i];
+                    break;
+                }
+
+                if (i == 0 && input[i] > input[i + 1])
+                {
+                    for (int j = 0; j < input.Length; j++)
+                    {
+                        if (input[i] > input[j])
+                        {
+
+                        }else if (input[i] < input[j])
+                        {
+                            a = false;
+                        }
+                    }
+                    if (a)
+                    {
+                        topIntegers[i] = input[i];
+                    }
+                    continue;
+                }
+
+                if (input[i] > input[i + 1])
+                {
+                    for (int j = i; j < input.Length; j++)
+                    {
+                        if (input[i] > input[j])
+                        {
+
+                        } else if (input[i] < input[j] || (input[i] == input[j] && i != j))
+                        {
+                            a = false;
+                        }
+                    }
+                    if (a)
+                    {
+                        topIntegers[i] = input[i];
+                    }
+                }
+                a = true;
+            }
+
+            for (int i = 0; i < topIntegers.Length; i++)
+            {
+                if (i != topIntegers.Length - 1 && topIntegers[i] == 0)
+                {
+                    continue;
+                }
+                Console.Write($"{topIntegers[i]} ");
+            }
+        }
+    }
+}
