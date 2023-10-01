c866136d0d41bcb526c00a885910da6c40d15759
Bozhidar1804 <bobo1804@abv.bg>
2023-10-01 15:20:51 +0300

    Solved Ex4 from ArraysExercise

---
+++ Fundamentals - September 2023/ArraysExercise/04. Array Rotation/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04._Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            string[] rotated = new string[input.Length];
+
+            for (int i = 0; i < n; i++)
+            {
+                rotated[input.Length - 1] = input[0];
+                for (int j = 0; j < input.Length - 1; j++)
+                {
+                    rotated[j] = input[j + 1];
+                }
+
+                for (int k = 0; k < input.Length; k++)
+                {
+                    input[k] = rotated[k];
+                }
+
+            }
+
+            Console.WriteLine(string.Join(" ", rotated));
+        }
+    }
+}
