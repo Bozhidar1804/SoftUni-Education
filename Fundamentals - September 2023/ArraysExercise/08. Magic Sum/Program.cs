27a8fe5c34aa87ea834b393009b37fbc4b1287a1
Bozhidar1804 <bobo1804@abv.bg>
2023-10-05 18:34:04 +0300

    Adding 2 solutions

---
+++ Fundamentals - September 2023/ArraysExercise/08. Magic Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+            string[] currentPair = new string[2];
+            string[] result = new string[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == input.Length - 1)
+                {
+                    continue;
+                }
+
+                for (int k = 0; k < input.Length; k++)
+                {
+                    if (input[i] + input[k] == n)
+                    {
+                        if (result.Contains(input[i].ToString()))
+                        {
+                            break;
+                        }
+                        string firstChar = input[i].ToString();
+                        string secondChar = input[k].ToString();
+                        currentPair[0] = firstChar;
+                        currentPair[1] = secondChar;
+                        result[i] = currentPair[0] + " " + currentPair[1];
+                    }
+                }
+            }
+
+            foreach(string s in result)
+            {
+                Console.Write($"{s} ");
+            }
+        }
+    }
+}
---
+++ Fundamentals - September 2023/MethodsLab/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SignOfNumber(n);
+        }
+
+        static void SignOfNumber(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive");
+            } else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            } else
+            {
+                Console.WriteLine($"The number {n} is zero.");
+            }
+        }
+    }
+}
