dfa7692c05f3782edc8348c16dd9da579a299230
Bozhidar1804 <bobo1804@abv.bg>
2023-10-05 18:41:14 +0300

    Solved Ex2 from MethodsLab

---
+++ Fundamentals - September 2023/MethodsLab/02. Grades/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02._Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Grade();
+        }
+
+        private static void Grade()
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            switch (grade)
+            {
+                case >= 2.00 and <= 2.99:
+                    Console.WriteLine("Fail");
+                    break;
+                case >= 3.00 and <= 3.49:
+                    Console.WriteLine("Poor");
+                    break;
+                case >= 3.50 and <= 4.49:
+                    Console.WriteLine("Good");
+                    break;
+                case >= 4.50 and <= 5.49:
+                    Console.WriteLine("Very good");
+                    break;
+                case >= 5.50 and <= 6.00:
+                    Console.WriteLine("Excellent");
+                    break;
+            }
+        }
+    }
+}
