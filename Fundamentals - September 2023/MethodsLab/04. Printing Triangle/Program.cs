de1ec1c7dbc2a3a5d0a87b92ae93aa9eae81943a
Bozhidar1804 <bobo1804@abv.bg>
2023-10-06 15:28:49 +0300

    Solved Ex4 from MethodsLab

---
+++ Fundamentals - September 2023/MethodsLab/04. Printing Triangle/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _04._Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write(col + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int row = n; row >= 1; row--)
+            {
+                for (int col = 1; col <= row - 1; col++)
+                {
+                    Console.Write(col + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
