eff7a3781a23691ddc2f52ba6c679182a7d353ef
Bozhidar1804 <bobo1804@abv.bg>
2023-10-07 14:32:34 +0300

    Solved Ex7 from MethodsLab

---
+++ Fundamentals - September 2023/MethodsLab/07. Repeat String/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            string result = Repetitions(input, n);
+            Console.WriteLine(result);
+        }
+
+        static string Repetitions(string text, int r)
+        {
+            string newText = "";
+            for (int i = 0; i < r; i++)
+            {
+                newText += text;
+            }
+            return newText;
+        }
+    }
+}
