1f1a9ac141ce06b3f7f2d939a0e04ea35e732d1b
Bozhidar1804 <bobo1804@abv.bg>
2023-10-07 14:43:34 +0300

    Solved Ex8 from MethodsLab

---
+++ Fundamentals - September 2023/MethodsLab/08. Math Power/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            double result = Power(number, power);
+            Console.WriteLine(result);
+        }
+
+        private static double Power(double num, double pow)
+        {
+            double result = 1;
+            for (int i = 0; i < pow; i++)
+            {
+                result = result * num;
+            }
+            return result;
+        }
+    }
+}
