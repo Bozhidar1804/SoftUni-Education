3679cec857995be99e0dd84cf62b4c005a4bbfd5
Bozhidar1804 <bobo1804@abv.bg>
2023-10-07 14:28:49 +0300

    Solved Ex6 from MethodsLab

---
+++ Fundamentals - September 2023/MethodsLab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _06._Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double result = CalculateRectangleArea(width, height);
+            Console.WriteLine(result);
+        }
+
+        private static double CalculateRectangleArea(double w, double h)
+        {
+            double result = w * h;
+            return result;
+        }
+    }
+}
