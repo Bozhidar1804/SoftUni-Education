44c332626a6d188f14e25393fc865c8ad749df1d
Bozhidar1804 <bobo1804@abv.bg>
2023-10-08 19:08:01 +0300

    Solved Ex6 from MethodsExercise

---
+++ Fundamentals - September 2023/MethodsExercise/06. Middle Characters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char resultChar;
+            string result;
+            if (input.Length % 2 == 0)
+            {
+                result = MiddleCharacters(input);
+                Console.WriteLine(result);
+
+            } else if (input.Length % 2 != 0)
+            {
+                resultChar = MiddleChar(input);
+                Console.WriteLine(resultChar);
+            }
+        }
+
+        private static char MiddleChar(string input)
+        {
+            return input[input.Length / 2];
+        }
+
+        private static string MiddleCharacters(string input)
+        {
+            return input.Substring((input.Length / 2) - 1, 2);
+        }
+    }
+}
