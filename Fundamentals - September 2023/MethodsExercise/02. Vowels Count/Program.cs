7b7189ca8377ed6d7b4f6606083c98ee18672ffd
Bozhidar1804 <bobo1804@abv.bg>
2023-10-08 14:04:52 +0300

    Solved Ex2 from MethodsEx

---
+++ Fundamentals - September 2023/MethodsExercise/02. Vowels Count/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int result = VowelsCount(input);
+            Console.WriteLine(result);
+
+        }
+
+        private static int VowelsCount(string a)
+        {
+            int count = 0;
+            a = a.ToLower();
+            foreach (char c in a)
+            {
+                if (IsVowel(c))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        static bool IsVowel(char c)
+        {
+            return "aeiou".Contains(c);
+        }
+    }
+}
