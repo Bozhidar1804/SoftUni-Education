507f961337cff8e46fe4d1f1458032bca58896ab
Bozhidar1804 <bobo1804@abv.bg>
2023-10-08 19:28:18 +0300

    Solved Ex8 from MethodsExercise

---
+++ Fundamentals - September 2023/MethodsExercise/08. Factorial Division/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = int.Parse(Console.ReadLine());
+            double secondNum = int.Parse(Console.ReadLine());
+
+            double result = Division(firstNum, secondNum);
+            Console.WriteLine($"{result:F2}");
+
+        }
+
+        private static double Division(double firstNum, double secondNum)
+        {
+            return Factorial(firstNum) / Factorial(secondNum);
+        }
+
+        private static double Factorial(double num)
+        {
+            double factorial = 1;
+            for (double i = 1; i <= num; i++)
+            {
+                factorial = factorial * i;
+            }
+            return factorial;
+        }
+    }
+}
