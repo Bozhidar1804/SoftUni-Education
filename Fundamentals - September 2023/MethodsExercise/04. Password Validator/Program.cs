b9be2d3366368e141b7361539081524b5dfa193f
Bozhidar1804 <bobo1804@abv.bg>
2023-10-08 18:55:47 +0300

    Solved Ex4 from MethodsEx (PROUD OF MYSELF), Solved Ex5 from MethodsEx

---
+++ Fundamentals - September 2023/MethodsExercise/04. Password Validator/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace _04._Password_Validator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool invalidPassword = true;
+
+            while(invalidPassword)
+            {
+                string input = Console.ReadLine();
+                if (IsPassValid(input))
+                {
+                    invalidPassword = false;
+                    Console.WriteLine("Password is valid");
+                }
+            }
+        }
+
+        private static bool IsPassValid(string input)
+        {
+            bool result = true;
+            if (!CharactersRange(input))
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+                result = false;
+            }
+            if (!IsOnlyLettersAndDigits(input))
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+                result = false;
+            }
+            if (!HasTwoDigits(input))
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+                result = false;
+            }
+            return result;
+        }
+
+        private static bool CharactersRange(string s)
+        {
+            int charactersCount = 0;
+            bool result = true;
+            foreach (char c in s)
+            {
+                charactersCount++;
+            }
+            if (charactersCount < 6 || charactersCount > 10)
+            {
+                result = false;
+                return result;
+            }
+            return result;
+        }
+
+        private static bool IsOnlyLettersAndDigits(string s)
+        {
+            foreach(char c in s)
+            {
+                if(!Char.IsLetter(c) && !Char.IsNumber(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool HasTwoDigits(string s)
+        {
+            int digitsCount = 0;
+            bool result = false;
+            foreach (char c in s)
+            {
+                if (Char.IsNumber(c))
+                {
+                    digitsCount++;
+                }
+            }
+            if (digitsCount >= 2)
+            {
+                result = true;
+                return result;
+            }
+            return result;
+        }
+    }
+}
---
+++ Fundamentals - September 2023/MethodsExercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int result = Subtract(Sum(firstNum, secondNum), thirdNum);
+            Console.WriteLine(result);
+        }
+
+        private static int Sum(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+
+        private static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
