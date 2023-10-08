a576be80e90397818d00075c5108cd0cf974df85
Bozhidar1804 <bobo1804@abv.bg>
2023-10-08 19:12:58 +0300

    Solved Ex7 from MethodsExercise

---
+++ Fundamentals - September 2023/MethodsExercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintMatrix(n);
+        }
+
+        private static void PrintMatrix(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(n + " ");
+                for (int j = 1; j < n; j++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
