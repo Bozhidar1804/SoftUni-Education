b634eca549661b741e4d74422ea1733da9eb496f
Bozhidar1804 <bobo1804@abv.bg>
2023-10-08 13:24:30 +0300

    Solved Ex1 from MethodsEx

---
+++ Fundamentals - September 2023/MethodsExercise/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int result = SmallestNumber(firstNum, secondNum, thirdNum);
+            Console.WriteLine(result);
+        }
+
+        private static int SmallestNumber(int a, int b, int c)
+        {
+            int result = 0;
+            if (a < b && a < c)
+            {
+                result = a;
+            }
+            else if (b < a && b < c)
+            {
+                result = b;
+            }
+            else if (c < a && c < b)
+            {
+                result = c;
+            }
+            return result;
+        }
+    }
+}
