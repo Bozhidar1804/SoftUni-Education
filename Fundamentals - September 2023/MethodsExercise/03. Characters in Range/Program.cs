7da5580319a0407457807f18f434a614f97f5081
Bozhidar1804 <bobo1804@abv.bg>
2023-10-08 14:21:10 +0300

    Solved Ex3 from MethodsExercise

---
+++ Fundamentals - September 2023/MethodsExercise/03. Characters in Range/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+
+            string result = CharInRange(a, b);
+            Console.WriteLine(result);
+        }
+
+        private static string CharInRange(char a, char b)
+        {
+            string result = "";
+            char bigger;
+            char smaller;
+            if (a > b)
+            {
+                smaller = b;
+                bigger = a;
+                for (char i = ++smaller; i < bigger; i++)
+                {
+                    result += i + " ";
+                }
+                return result;
+            }
+
+
+            for (char i = ++a; i < b; i++)
+            {
+                result += i + " ";
+            }
+            return result;
+        }
+    }
+}
