262a0dba5b53bab9971dde15372cfb6c6d832315
Bozhidar1804 <bobo1804@abv.bg>
2023-10-10 01:23:27 +0300

    Solved Ex9 from MethodsExercise

---
+++ Fundamentals - September 2023/MethodsExercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runningProgram = true;
+            while(runningProgram)
+            {
+                string currentInput = Console.ReadLine();
+                if (currentInput == "END")
+                {
+                    runningProgram = false;
+                    continue;
+                }
+
+                IsPalindromeInt(currentInput);
+            }
+        }
+
+        private static bool IsPalindromeInt(string input)
+        {
+            bool result = false;
+            char[] inputArray = input.ToCharArray();
+            Array.Reverse(inputArray);
+            string reversed = new string(inputArray);
+
+            if (input == reversed)
+            {
+                result = true;
+                Console.WriteLine("true");
+                return result;
+            }
+            Console.WriteLine("false");
+            return result;
+        }
+    }
+}
