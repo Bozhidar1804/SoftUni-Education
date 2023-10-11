a463a97a4b8579f8001ef5ed85314558a6d38d49
Bozhidar1804 <bobo1804@abv.bg>
2023-10-12 01:11:34 +0300

    Solved Ex3 from ListsLab

---
+++ Fundamentals - September 2023/ListsLab/03. Merging Lists/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _03._Merging_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> resultList = MergeTwoLists(firstList, secondList);
+            Console.WriteLine(string.Join(" ", resultList));
+        }
+
+        private static List<int> MergeTwoLists(List<int> firstList, List<int> secondList)
+        {
+            List<int> shorterList;
+            List<int> longerList;
+            List<int> resultList = new List<int>();
+
+            if (firstList.Count > secondList.Count)
+            {
+                shorterList = secondList;
+                longerList = firstList;
+            }
+            else
+            {
+                shorterList = firstList;
+                longerList = secondList;
+            }
+
+            for (int i = 0; i < shorterList.Count; i++)
+            {
+                resultList.Add(firstList[i]);
+                resultList.Add(secondList[i]);
+
+                firstList.RemoveAt(i);
+                secondList.RemoveAt(i);
+                i = -1;
+            }
+
+            for (int i = 0; i < longerList.Count; i++)
+            {
+                resultList.Add(longerList[i]);
+
+                longerList.RemoveAt(i);
+                i = -1;
+            }
+
+            return resultList;
+        }
+    }
+}
