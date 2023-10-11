5d4ad4b65ae735291bdb092bddec84a481a7b5c5
Bozhidar1804 <bobo1804@abv.bg>
2023-10-12 00:48:58 +0300

    Solved Ex2 from ListsLab

---
+++ Fundamentals - September 2023/ListsLab/02. Gauss' Trick/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _02._Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList(); // 1 2 3 4 5
+            numbers = Gauss(numbers);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static List<int> Gauss(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                numbers[i] = numbers[i] + numbers[numbers.Count - 1];
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+            return numbers;
+        }
+    }
+}
