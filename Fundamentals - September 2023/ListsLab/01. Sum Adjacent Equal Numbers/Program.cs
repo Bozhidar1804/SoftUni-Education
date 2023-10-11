eedcdfbf3892b5c1592c14664695fcc949d7c844
Bozhidar1804 <bobo1804@abv.bg>
2023-10-12 00:35:01 +0300

    Solved Ex1 from ListsLab

---
+++ Fundamentals - September 2023/ListsLab/01. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01._Sum_Adjacent_Equal_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+            numbers = SumEqualNumbers(numbers);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static List<double> SumEqualNumbers(List<double> numbers)
+        {
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbers[i] = numbers[i] + numbers[i + 1];
+                    numbers.RemoveAt(i + 1);
+
+                    i = -1;
+                }
+            }
+            return numbers;
+        }
+    }
+}
