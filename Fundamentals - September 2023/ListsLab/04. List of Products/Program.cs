d27034c584fdc15d233a73615c1150db5c80cc8b
Bozhidar1804 <bobo1804@abv.bg>
2023-10-12 23:37:12 +0300

    Solved Ex4 from ListLab

---
+++ Fundamentals - September 2023/ListsLab/04. List of Products/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _04._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            ListProducts(n);
+        }
+
+        private static void ListProducts(int n)
+        {
+            List<string> products = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string currentProduct = Console.ReadLine();
+                products.Add(currentProduct);
+            }
+            products.Sort();
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
