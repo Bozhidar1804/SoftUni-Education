f6cf81f22632081bdb10d4299d9f0036e3877e67
Bozhidar1804 <bobo1804@abv.bg>
2023-10-15 21:26:43 +0300

    Updated Ex6 from ListsExercise

--- Fundamentals - September 2023/ListsExercise/06. Cards Game/Program.cs
+++ Fundamentals - September 2023/ListsExercise/06. Cards Game/Program.cs
@@ -6,7 +6,11 @@
         {
             List<int> firstDeck = Console.ReadLine().Split().Select(int.Parse).ToList();
             List<int> secondDeck = Console.ReadLine().Split().Select(int.Parse).ToList();
+            CompareDecks(firstDeck, secondDeck);
+        }
 
+        private static void CompareDecks(List<int> firstDeck, List<int> secondDeck)
+        {
             while (firstDeck.Any() && secondDeck.Any())
             {
                 if (firstDeck[0] > secondDeck[0])
@@ -15,13 +19,15 @@
                     firstDeck.Insert(firstDeck.Count - 1, firstDeck[0]);
                     firstDeck.RemoveAt(0);
                     secondDeck.RemoveAt(0);
-                } else if (secondDeck[0] > firstDeck[0])
+                }
+                else if (secondDeck[0] > firstDeck[0])
                 {
                     secondDeck.Add(firstDeck[0]);
                     secondDeck.Insert(secondDeck.Count - 1, secondDeck[0]);
                     secondDeck.RemoveAt(0);
                     firstDeck.RemoveAt(0);
-                } else if (firstDeck[0] == secondDeck[0])
+                }
+                else if (firstDeck[0] == secondDeck[0])
                 {
                     firstDeck.RemoveAt(0);
                     secondDeck.RemoveAt(0);
@@ -31,7 +37,8 @@
             if (firstDeck.Any())
             {
                 Console.WriteLine($"First player wins! Sum: {firstDeck.Sum()}");
-            } else if (secondDeck.Any())
+            }
+            else if (secondDeck.Any())
             {
                 Console.WriteLine($"Second player wins! Sum: {secondDeck.Sum()}");
             }
