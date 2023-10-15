7a4087b52437f0a75f61dc010e9e4a3f7664fbf6
Bozhidar1804 <bobo1804@abv.bg>
2023-10-15 16:06:08 +0300

    Solved Ex2 and 3 from ListsEx

---
+++ Fundamentals - September 2023/ListsExercise/02. Change List/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _02._Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> result = ChangeList(ints);
+            foreach(int i in result)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+
+        private static List<int> ChangeList(List<int> ints)
+        {
+            string command = "";
+
+            while (command != "end")
+            {
+                command = Console.ReadLine();
+                if (command == "end")
+                {
+                    continue;
+                }
+
+                if (command.Contains("Delete"))
+                {
+                    string[] splitCommand = command.Split();
+                    int delete = int.Parse(splitCommand[1]);
+                    for (int i = 0; i < ints.Count; i++)
+                    {
+                        if (ints[i] == delete)
+                        {
+                            ints.RemoveAt(i);
+                        }
+                    }
+                }
+                else if (command.Contains("Insert"))
+                {
+                    string[] splitCommand = command.Split();
+                    int number = int.Parse(splitCommand[1]);
+                    int position = int.Parse(splitCommand[2]);
+                    ints.Insert(position, number);
+                }
+            }
+
+            return ints;
+        }
+    }
+}
---
+++ Fundamentals - September 2023/ListsExercise/03. House Party/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _03._House_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int commands = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>();
+            HouseParty(commands, names);
+        }
+
+        private static void HouseParty(int commands, List<string> names)
+        {
+            int count = 0;
+            while (count < commands)
+            {
+                string command = Console.ReadLine();
+                string[] commandSplit = command.Split();
+                string name = commandSplit[0];
+                if (command.Contains("is going!") && !command.Contains("not") && !names.Contains(name))
+                {
+                    names.Add(name);
+                }
+                else if (names.Contains(name) && !command.Contains("not"))
+                {
+                    Console.WriteLine($"{name} is already in the list!");
+                }
+                else if (command.Contains("is not going!"))
+                {
+                    if (names.Contains(name))
+                    {
+                        names.Remove(name);
+                        break;
+                    }
+                    Console.WriteLine($"{name} is not in the list!");
+                    names.Remove(name);
+                }
+                count++;
+            }
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
