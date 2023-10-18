303b32f8b85faf6ce1c1c4a4ea9d1b47ee66ac52
Bozhidar1804 <bobo1804@abv.bg>
2023-10-18 23:53:05 +0300

    Practiced for Mid Exams

---
+++ Fundamentals - September 2023/MidExamPreparation/01. Guinea Pig/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _01._Guinea_Pig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double foodKg = double.Parse(Console.ReadLine());
+            double food = foodKg * 1000;
+            double hayKg = double.Parse(Console.ReadLine());
+            double hay = hayKg * 1000;
+            double coverKg = double.Parse(Console.ReadLine());
+            double cover = coverKg * 1000;
+            double weightKg = double.Parse((Console.ReadLine()));
+            double weight = weightKg * 1000;
+
+            double hayGiven = 0;
+            double coverGiven = 0;
+
+            for (int i = 1; i <= 30; i++)
+            {
+                food -= 300;
+                if (i % 2 == 0)
+                {
+                    hayGiven = food * 0.05;
+                    hay -= hayGiven;
+                }
+                
+                if (i % 3 == 0)
+                {
+                    coverGiven = weight / 3;
+                    cover -= coverGiven;
+                }
+
+                if (food <= 0 || hay <= 0 || cover <= 0)
+                {
+                    Console.WriteLine("Merry must go to the pet store!");
+                    return;
+                } 
+            }
+
+            if (food >= 0 && hay >= 0 && cover >= 0)
+            {
+                Console.WriteLine($"Everything is fine! Puppy is happy! Food: {(food / 1000):F2}, Hay: {(hay / 1000):F2}, Cover: {(cover / 1000):F2}.");
+            }
+        }
+    }
+}
---
+++ Fundamentals - September 2023/MidExamPreparation/02. The Lift/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _02._The_Lift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int queue = int.Parse(Console.ReadLine());
+            int[] liftState = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int wagons = liftState.Length;
+            int index = 0;
+
+            while(!WagonsFull(liftState, wagons, queue))
+            {
+                for (int j = liftState[index]; j < 4; j++)
+                {
+                    liftState[index] += 1;
+                    queue--;
+                    if (queue == 0)
+                    {
+                       break;
+                    }
+                }
+
+                index++;
+            }
+
+            if (queue > 0)
+            {
+                Console.WriteLine($"There isn't enough space! {queue} people in a queue!");
+                foreach (int i in liftState)
+                {
+                    Console.Write($"{i} ");
+                }
+            } else if (queue == 0 || queue < 0)
+            {
+                Console.WriteLine("The lift has empty spots!");
+                foreach (int i in liftState)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+
+        private static bool WagonsFull(int[] liftState, int wagons, int queue)
+        {
+            if (queue == 0)
+            {
+                return true;
+            }
+            bool result = true; ;
+            for (int i = 0; i < wagons; i++)
+            {
+                if (liftState[i] != 4)
+                {
+                    result = false;
+                    return result;
+                }
+            }
+
+            return result;
+        }
+    }
+}
