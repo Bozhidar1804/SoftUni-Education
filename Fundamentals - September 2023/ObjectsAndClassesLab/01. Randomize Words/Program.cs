100cba23266bcac5e86cca8838f3612347be1324
Bozhidar1804 <bobo1804@abv.bg>
2023-10-26 22:06:12 +0300

    Solved Ex1 and Ex2 from ObjectsAndClassesLab

---
+++ Fundamentals - September 2023/ObjectsAndClassesLab/01. Randomize Words/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+
+            Random rnd = new Random();
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                int randomIndex = rnd.Next(input.Length);
+
+                string currentWord = input[i];
+                string randomWord = input[randomIndex];
+
+                input[i] = randomWord;
+                input[randomIndex] = currentWord;
+            }
+
+            foreach (string s in input)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
