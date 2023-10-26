394fa39554e43d8b35fc30c3734c6c141ec9fc5e
Bozhidar1804 <bobo1804@abv.bg>
2023-10-27 00:25:10 +0300

    Solved Ex4 from ObjectsAndClassesLab

---
+++ Fundamentals - September 2023/ObjectsAndClassesLab/04. Students/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Data;
+
+namespace _04._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            string command = "";
+
+            while (command != "end")
+            {
+                command = Console.ReadLine();
+                if (command == "end") break;
+
+                string[] input = command.Split(" ");
+
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                string homeTown = input[3];
+
+                Student student = new Student(firstName, lastName, age, homeTown);
+                students.Add(student);
+            }
+
+            string town = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (student.HomeTown == town)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+    }
+
+    class Student
+    {
+        public Student (string firstName, string lastName, int age, string homeTown)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            HomeTown = homeTown;
+        }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string HomeTown { get; set; }
+    }
+}
