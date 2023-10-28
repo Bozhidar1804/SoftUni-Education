a9d6e06d4ada2f062903c1f94f449a8b76eb13a0
Bozhidar1804 <bobo1804@abv.bg>
2023-10-28 16:05:38 +0300

    Solved Ex4 from ObjectsAndClassesExercise and prepared Ex3 for tomorrow

---
+++ Fundamentals - September 2023/ObjectsAndClassesExercise/03. Articles 2.0/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _03._Articles_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Article
+    {
+
+    }
+}
---
+++ Fundamentals - September 2023/ObjectsAndClassesExercise/04. Students/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography;
+
+namespace _04._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(" ");
+
+                string firstName = command[0];
+                string lastName = command[1];
+                float grade = float.Parse(command[2]);
+
+                Student student = new Student(firstName, lastName, grade);
+                students.Add(student);
+            }
+
+            List<Student> descendedStudents = new List<Student>(students.OrderByDescending(g => g.Grade));
+
+            foreach(Student student in descendedStudents)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:F2}");
+            }
+        }
+    }
+
+    class Student
+    {
+        public Student(string firstName, string lastName, float grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+        public string FirstName { get; set; } 
+
+        public string LastName { get; set; }
+
+        public float Grade { get; set; }
+    }
+}
