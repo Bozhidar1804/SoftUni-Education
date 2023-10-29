19ab48fbacb1088ec15b4149079e5f301f4d956c
Bozhidar1804 <bobo1804@abv.bg>
2023-10-29 13:47:27 +0200

    Solved Ex3 from ObjectsAndClassesExercise

--- Fundamentals - September 2023/ObjectsAndClassesExercise/03. Articles 2.0/Program.cs
+++ Fundamentals - September 2023/ObjectsAndClassesExercise/03. Articles 2.0/Program.cs
@@ -4,12 +4,38 @@
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            List<Article> articles = new List<Article>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(", ");
+                string title = input[0];
+                string content = input[1];
+                string author = input[2];
+                Article article = new Article(title, content, author);
+                articles.Add(article);
+            }
+
+            foreach (Article article in articles)
+            {
+                Console.WriteLine($"{article.Title} - {article.Content}: {article.Author}");
+            }
         }
     }
 
     class Article
     {
+        public Article (string title, string content, string author)        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
 
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
     }
 }
