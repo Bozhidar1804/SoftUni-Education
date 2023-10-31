b2f23769e6490a05c16a509b281a90447b42a883
Bozhidar1804 <bobo1804@abv.bg>
2023-10-31 15:02:47 +0200

    Solved Ex7 from ObjectsAndClassesExercise

---
+++ Fundamentals - September 2023/ObjectsAndClassesExercise/07. Order by Age/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace _07._Order_by_Age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            string command = "";
+
+            while (command != "End")
+            {
+                command = Console.ReadLine();
+                if (command == "End") break;
+
+
+                string[] commandArr = command.Split(' ');
+                string name = commandArr[0];
+                int id = int.Parse(commandArr[1]);
+                int age = int.Parse(commandArr[2]);
+
+                if (idExists(people, id))
+                {
+                    foreach(Person person in people)
+                    {
+                        if (person.ID == id)
+                        {
+                            person.Name = name;
+                            person.Age = age;
+                        }
+                    }
+                } else
+                {
+                    Person person = new Person(name, id, age);
+                    people.Add(person);
+                }
+            }
+
+            List<Person> orderedPeople = people.OrderBy(x => x.Age).ToList();
+
+            foreach (Person person in orderedPeople)
+            {
+                Console.WriteLine($"{person.Name} with ID: {person.ID} is {person.Age} years old.");
+            }
+        }
+
+        public static bool idExists (List<Person> people, int id)
+        {
+            bool result = false;
+
+            foreach (Person person in people)
+            {
+                if (id == person.ID)
+                {
+                    result = true;
+                }
+            }
+
+
+            return result;
+        }
+    }
+
+    class Person
+    {
+        public Person (string name, int id, int age)
+        {
+            Name = name;
+            ID = id;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int ID { get; set; } 
+
+        public int Age { get; set; }
+    }
+}
