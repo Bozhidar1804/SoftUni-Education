113a15269ce74580da6e4f403835b365db909058
Bozhidar1804 <bobo1804@abv.bg>
2023-11-01 18:52:03 +0200

    Solved Ex3 from AssociativeArraysLab

---
+++ Fundamentals - September 2023/AssociativeArraysLab/03. Word Synonyms/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!words.ContainsKey(word))
+                {
+                    words[word] = new List<string>() { synonym };
+                } else
+                {
+                    words[word].Add(synonym);
+                }
+            }
+
+            foreach(KeyValuePair<string, List<string>> kvp in words)
+            {
+
+                Console.WriteLine($"{kvp.Key} - {string.Join(", ", kvp.Value)}");
+            }
+
+        }
+    }
+}
