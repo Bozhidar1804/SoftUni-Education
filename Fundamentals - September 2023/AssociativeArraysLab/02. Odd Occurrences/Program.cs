0614cdaf75060b3c1b82ffb4d8ce59274f8c0900
Bozhidar1804 <bobo1804@abv.bg>
2023-11-01 18:23:33 +0200

    Solved Ex2 from AssociativeArraysLab

---
+++ Fundamentals - September 2023/AssociativeArraysLab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ");
+
+            Dictionary<string, int> appearances = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string wordInLowerCase = word.ToLower();
+                if (appearances.ContainsKey(wordInLowerCase))
+                {
+                    appearances[wordInLowerCase]++;
+                } else
+                {
+                    appearances.Add(wordInLowerCase, 1);
+                }
+            }
+
+            string result = "";
+
+            foreach (KeyValuePair<string, int> kvp in appearances)
+            {
+                if (kvp.Value % 2 != 0)
+                {
+                    result += kvp.Key + " ";
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
