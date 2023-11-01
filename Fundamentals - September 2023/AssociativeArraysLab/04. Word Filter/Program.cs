7c1d786e2dd1613ce540c92ada9751c9ff4ac8b8
Bozhidar1804 <bobo1804@abv.bg>
2023-11-01 19:04:47 +0200

    Solved Ex4 from AssociativeArraysLab

---
+++ Fundamentals - September 2023/AssociativeArraysLab/04. Word Filter/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ").Where(w => w.Length % 2 == 0).ToArray();
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
