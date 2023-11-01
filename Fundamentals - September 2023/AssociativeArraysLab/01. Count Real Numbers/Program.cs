6f084fce05ae40ce8a1e55442b9a96cfc3cff4e2
Bozhidar1804 <bobo1804@abv.bg>
2023-11-01 14:17:52 +0200

    Solved Ex1 from AssociativeArraysLab

---
+++ Fundamentals - September 2023/AssociativeArraysLab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+            foreach (int number in numbers)
+            {
+                if(!counts.ContainsKey(number))
+                {
+                    counts[number] = 1;
+                } else
+                {
+                    counts[number]++;
+                }
+            }
+
+            foreach (KeyValuePair<double, int> kvp in counts)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
