89b31330d087f3011ed06a0717f7a50432d11735
Bozhidar1804 <bobo1804@abv.bg>
2023-11-04 15:59:42 +0200

    Solved Ex2 from AssociativeArraysExercise

---
+++ Fundamentals - September 2023/AssociativeArraysExercise/02. A Miner Task/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            string command = "";
+
+            while (command != "stop")
+            {
+                command = Console.ReadLine();
+                if (command == "stop") break;
+
+                string resource = command;
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resources.ContainsKey(resource))
+                {
+                    resources.Add(resource, quantity);
+                } else
+                {
+                    resources[resource] += quantity;
+                }
+            }
+
+            foreach (KeyValuePair<string, int> kvp in resources)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+            
+        }
+    }
+}
