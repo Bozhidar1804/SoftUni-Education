5a52cf6ef91ba3038dc91b6dae8a575ab6c51364
Bozhidar1804 <bobo1804@abv.bg>
2023-11-04 15:54:42 +0200

    Solved Ex1 from AssociativeArraysExercise

---
+++ Fundamentals - September 2023/AssociativeArraysExercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ").ToArray();
+            Dictionary<char, int> letterOcc = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string currentWord = input[i];
+
+                foreach (char c in currentWord)
+                {
+                    if (!letterOcc.ContainsKey(c))
+                    {
+                        letterOcc.Add(c, 1);
+                    } else
+                    {
+                        letterOcc[c]++;
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<char, int> kvp in letterOcc)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
