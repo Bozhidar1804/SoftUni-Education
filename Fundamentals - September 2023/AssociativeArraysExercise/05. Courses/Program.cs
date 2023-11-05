775c3930a09c322d1f4858156cdc66f2f1fc0de4
Bozhidar1804 <bobo1804@abv.bg>
2023-11-05 17:49:02 +0200

    Solved Ex5 from AssociativeArraysExercise

---
+++ Fundamentals - September 2023/AssociativeArraysExercise/05. Courses/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _05._Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+            string command = "";
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] commandArr = command.Split(" : ");
+                string courseName = commandArr[0];
+                string currentStudent = commandArr[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses.Add(courseName, new List<string>());
+                    courses[courseName].Add(currentStudent);
+                } else
+                {
+                    courses[courseName].Add(currentStudent);
+                }
+            }
+
+            foreach (KeyValuePair<string, List<string>> kvp in courses)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Count}");
+                foreach (string student in kvp.Value)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
