918d8050f5354d15c92e15568c9ddcb0bd57dd8f
Bozhidar1804 <bobo1804@abv.bg>
2023-11-05 14:30:53 +0200

    Solved Ex4 from AssociativeArraysExercise

---
+++ Fundamentals - September 2023/AssociativeArraysExercise/04. SoftUni Parking/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _04._SoftUni_Parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, string> registeredUsers = new Dictionary<string, string>();
+            int counter = 0;
+
+            while (counter < n)
+            {
+                string[] commandArr = Console.ReadLine().Split(" ");
+                string currentCommand = commandArr[0];
+                string username = commandArr[1];
+
+                if (currentCommand == "register")
+                {
+                    string licensePlate = commandArr[2];
+                    if (!registeredUsers.ContainsKey(username))
+                    {
+                        registeredUsers.Add(username, licensePlate);
+                        Console.WriteLine($"{username} registered {licensePlate} successfully");
+                    } else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlate}");
+                    }
+
+                } else if (currentCommand == "unregister")
+                {
+                    if (!registeredUsers.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    } else
+                    {
+                        registeredUsers.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                }
+                counter++;
+            }
+
+            foreach (KeyValuePair<string, string> kvp in registeredUsers)
+            {
+                Console.WriteLine($"{kvp.Key} => {kvp.Value}");
+            }
+        }
+    }
+}
