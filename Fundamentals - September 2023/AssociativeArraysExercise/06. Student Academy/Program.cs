8129925e13ad60a4269b7afd65f740dcd3e72cd4
Bozhidar1804 <bobo1804@abv.bg>
2023-11-05 18:22:36 +0200

    Solved Ex6 from AssociativeArraysExercise

---
+++ Fundamentals - September 2023/AssociativeArraysExercise/06. Student Academy/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _06._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> info = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string currentStudent = Console.ReadLine();
+                double currentGrade = double.Parse(Console.ReadLine());
+
+                if (!info.ContainsKey(currentStudent))
+                {
+                    info[currentStudent] = new List<double>();
+                    info[currentStudent].Add(currentGrade);
+                } else
+                {
+                    info[currentStudent].Add(currentGrade);
+                }
+            }
+
+            double average = 0;
+            double sum = 0;
+
+            foreach(KeyValuePair<string, List<double>> kvp in info)
+            {
+                foreach (double grade in kvp.Value)
+                {
+                    sum += grade;
+                }
+                average = sum / kvp.Value.Count;
+
+                if (average < 4.50)
+                {
+                    info.Remove(kvp.Key);
+                } else
+                {
+                    Console.WriteLine($"{kvp.Key} -> {average:F2}");
+                }
+
+                average = 0;
+                sum = 0;
+            }
+        }
+    }
+}
