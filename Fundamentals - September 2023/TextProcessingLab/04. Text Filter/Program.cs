2a47a1c577243e61aa55fb3327696de421d63713
Bozhidar1804 <bobo1804@abv.bg>
2023-11-09 00:04:34 +0200

    Solved Ex4 from TextProcessingLab

---
+++ Fundamentals - September 2023/TextProcessingLab/04. Text Filter/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string inputText = Console.ReadLine();
+            string result = "";
+
+            foreach (string word in bannedWords)
+            {
+                char asterisk = '*';
+                int count = word.Length;
+
+                string replacement = new string(asterisk, count);
+                result = inputText.Replace(word, replacement);
+                inputText = result;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
