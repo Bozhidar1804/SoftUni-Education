2fcada814e79e959bc7a22e10860fd12e339b33c
Bozhidar1804 <bobo1804@abv.bg>
2023-11-08 23:28:25 +0200

    Solved Ex1 from TextProcessingLab

---
+++ Fundamentals - September 2023/TexProcessingLab/01. Reverse Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string reversed = "";
+
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reversed += input[i];
+                }
+
+                Console.WriteLine($"{input} = {reversed}");
+            }
+        }
+    }
+}
