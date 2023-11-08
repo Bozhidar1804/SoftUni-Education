754705a165521524018df0bf42340837c2cfec1b
Bozhidar1804 <bobo1804@abv.bg>
2023-11-08 23:34:11 +0200

    test

---
+++ Fundamentals - September 2023/TextProcessingLab/02. Repeat Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            StringBuilder output = new StringBuilder();
+
+            foreach (string word in input)
+            {
+                int length = word.Length;
+
+                for (int i = 0; i < length; i++)
+                {
+                    output.Append(word);
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
