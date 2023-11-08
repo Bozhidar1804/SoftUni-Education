89edd6f859d7a6c62c06cb282f3f951b340ea985
Bozhidar1804 <bobo1804@abv.bg>
2023-11-08 23:50:04 +0200

    Solved Ex3 from TextProcessingLab

---
+++ Fundamentals - September 2023/TextProcessingLab/03. Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstStr = Console.ReadLine();
+            string secondStr = Console.ReadLine();
+            string result = "";
+
+            while (secondStr.Contains(firstStr))
+            {
+                int length = firstStr.Length;
+                int index = secondStr.IndexOf(firstStr, 0);
+
+                result = secondStr.Remove(index, length);
+                secondStr = result;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
