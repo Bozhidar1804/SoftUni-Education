f49697cadb74aef226e144aae03d52ee2f41a83f
Bozhidar1804 <bobo1804@abv.bg>
2023-11-09 00:10:09 +0200

    Solved Ex5 from TextProcessingLab

---
+++ Fundamentals - September 2023/TextProcessingLab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string resultDigits = "";
+            string resultLetters = "";
+            string resultOthers = "";
+
+            foreach (char c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    resultDigits += c;
+                } else if (char.IsLetter(c))
+                {
+                    resultLetters += c;
+                } else
+                {
+                    resultOthers += c;
+                }
+            }
+
+            Console.WriteLine(resultDigits);
+            Console.WriteLine(resultLetters);
+            Console.WriteLine(resultOthers);
+        }
+    }
+}
