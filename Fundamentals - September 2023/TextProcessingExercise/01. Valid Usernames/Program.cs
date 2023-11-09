c4e0befca1b7c2b533799f4c64d40655b6d64f19
Bozhidar1804 <bobo1804@abv.bg>
2023-11-09 16:44:54 +0200

    Solved Ex1 from TextProcessingExercise

---
+++ Fundamentals - September 2023/TextProcessingExercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ");
+            List<string> result = new List<string>();
+
+            foreach(string s in input)
+            {
+                if (s.Length > 3 && s.Length < 16)
+                {
+                    bool validWord = true;
+                    foreach (char c in s)
+                    {
+                        if (!char.IsLetterOrDigit(c) && !(c == '-' || c == '_'))
+                        {
+                            validWord = false;
+                            break;
+                        }
+                    }
+                    if (validWord)
+                    {
+                        result.Add(s);
+                    }
+                }
+            }
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
