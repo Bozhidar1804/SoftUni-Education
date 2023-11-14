dafc656d3d1ea886f480fdef42110573b6bd0f73
Bozhidar1804 <bobo1804@abv.bg>
2023-11-14 16:16:00 +0200

    Solved Ex3 from TextProcessingExercise

---
+++ Fundamentals - September 2023/TextProcessingExercise/03. Extract File/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string fileName = string.Empty;
+            string fileExtension = string.Empty;
+
+            int LastSeparatorIndex = input.LastIndexOf('\\');
+            int LastSeparatorIndex2 = input.LastIndexOf('.');
+
+            string[] inputArr = input.Split(input[LastSeparatorIndex], input[LastSeparatorIndex2]).ToArray();
+            fileName = inputArr[inputArr.Length - 2];
+            fileExtension = inputArr[inputArr.Length - 1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
