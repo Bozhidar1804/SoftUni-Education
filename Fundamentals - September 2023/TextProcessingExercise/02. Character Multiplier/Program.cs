f027aae803a3e4d4ecf029410ae6c9e99e5af6d7
Bozhidar1804 <bobo1804@abv.bg>
2023-11-14 16:01:16 +0200

    Solved Ex2 from TextProcessingEx

---
+++ Fundamentals - September 2023/TextProcessingExercise/02. Character Multiplier/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02._Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            Console.WriteLine(Sum(input[0], input[1]));
+
+        }
+
+        public static int Sum(string first, string second)
+        {
+            int sum = 0;
+
+            int length = Math.Max(first.Length, second.Length);
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i < first.Length && i < second.Length)
+                {
+                    sum += first[i] * second[i];
+                } else if (i < first.Length)
+                {
+                    sum += first[i];
+                } else if (i < second.Length)
+                {
+                    sum += second[i];
+                }
+            }
+            return sum;
+        }
+    }
+}
