303874bdd066ff24e7b6aa7301e084990f1c4b90
Bozhidar1804 <bobo1804@abv.bg>
2023-11-15 16:02:22 +0200

    Solved Ex4 from TextProcessingExercise

---
+++ Fundamentals - September 2023/TextProcessingExercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder cipherBuilder = new StringBuilder();
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                char original = input[i];
+                cipherBuilder.Append((char)(original + 3));
+            }
+
+            Console.WriteLine(cipherBuilder);
+        }
+    }
+}
