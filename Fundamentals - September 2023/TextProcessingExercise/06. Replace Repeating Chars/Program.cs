922f58efe7d065f764cd0aa555366bf83ce622b1
Bozhidar1804 <bobo1804@abv.bg>
2023-11-17 00:22:41 +0200

    Solved Ex6 from TextProcessingExercise

---
+++ Fundamentals - September 2023/TextProcessingExercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+            result.Append(input[0]);
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] != input[i - 1])
+                {
+                    result.Append(input[i]);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
