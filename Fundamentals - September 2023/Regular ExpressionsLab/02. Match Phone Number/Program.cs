486f468f685524f2de176fb458186ea6edf32d1d
Bozhidar1804 <bobo1804@abv.bg>
2023-11-17 22:50:02 +0200

    Solved Ex2 from RegExLab

---
+++ Fundamentals - September 2023/Regular ExpressionsLab/02. Match Phone Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359([ -])2\1\d{3}\1\d{4}\b";
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            string[] matchedPhones = matches.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
