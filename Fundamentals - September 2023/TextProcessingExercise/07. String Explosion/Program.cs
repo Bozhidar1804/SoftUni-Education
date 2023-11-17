5cab815ea4d9107c5ec762677a28e8b14e1f5db7
Bozhidar1804 <bobo1804@abv.bg>
2023-11-17 21:03:28 +0200

    Solved Ex7 from TextProcessingExercise

---
+++ Fundamentals - September 2023/TextProcessingExercise/07. String Explosion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace _07._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = StringExplosion(input);
+            Console.WriteLine(result);
+        }
+
+        public static string StringExplosion (string input)
+        {
+            StringBuilder result = new StringBuilder();
+
+            int strength = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    strength += int.Parse(input[i + 1].ToString());
+                    result.Append(input[i]);
+                } else if (strength == 0)
+                {
+                    result.Append(input[i]);
+                } else
+                {
+                    strength--;
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
