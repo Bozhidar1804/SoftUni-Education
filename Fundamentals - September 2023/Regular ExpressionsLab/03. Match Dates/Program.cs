5c8f8b4d4e7fe0182b06d0e55691050b42b89d77
Bozhidar1804 <bobo1804@abv.bg>
2023-11-17 23:30:18 +0200

    Solved Ex3 from RegExLab

---
+++ Fundamentals - September 2023/Regular ExpressionsLab/03. Match Dates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection dates = regex.Matches(input);
+
+            foreach (Match date in dates)
+            {
+                string day = date.Groups["day"].Value;
+                string month = date.Groups["month"].Value;
+                string year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
