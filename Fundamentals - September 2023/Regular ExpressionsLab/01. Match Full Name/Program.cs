610d4a02ad6b6c67eda8b7631ac8f23e165aa0e9
Bozhidar1804 <bobo1804@abv.bg>
2023-11-17 22:03:01 +0200

    Solved Ex1 from RegexLab

---
+++ Fundamentals - September 2023/Regular ExpressionsLab/01. Match Full Name/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.Write($"{match} ");
+            }
+        }
+    }
+}
