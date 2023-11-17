440e6a57cb0ffaa910b94844904cc3de83da40da
Bozhidar1804 <bobo1804@abv.bg>
2023-11-18 01:07:48 +0200

    Solved Ex1 from RegExEx

---
+++ Fundamentals - September 2023/RegularExpressionsExercise/01. Furniture/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>(?<product>[A-Za-z]+)<<(?<price>\d+(\.\d+)?)!(?<quantity>\d+)\b";
+            string command = "";
+            
+            Regex regex = new Regex(pattern);
+
+            List<string> boughtFurniture = new List<string>();
+            double totalPrice = 0;
+            bool isValid = false;
+
+            while ((command = Console.ReadLine()) != "Purchase")
+            {
+                Match product = regex.Match(command);
+
+                if (product.Success)
+                {
+                    double price = double.Parse(product.Groups["price"].Value);
+                    int quantity = int.Parse(product.Groups["quantity"].Value);
+                    totalPrice += quantity * price;
+
+                    boughtFurniture.Add(product.Groups["product"].Value);
+                } else
+                {
+                    continue;
+                }
+            }
+
+            Console.WriteLine("Bought furniture:");
+            foreach (string product in boughtFurniture)
+            {
+                Console.WriteLine(product);
+            }
+
+            Console.WriteLine($"Total money spend: {totalPrice:F2}");
+        }
+    }
+}
