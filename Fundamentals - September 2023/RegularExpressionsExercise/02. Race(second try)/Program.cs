9f1e1d2cf600438ca4bad9525a7dd73c731f7cb6
Bozhidar1804 <bobo1804@abv.bg>
2023-11-18 02:46:15 +0200

    Solved Ex2 from RegExEx

---
+++ Fundamentals - September 2023/RegularExpressionsExercise/02. Race(second try)/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Race_second_try_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> validParticipants = Console.ReadLine().Split(", ").ToList();
+            List<Participant> participants = new List<Participant>();
+
+            foreach (string name in validParticipants)
+            {
+                Participant participant = new Participant(name);
+                participants.Add(participant);
+            }
+
+            string lettersPattern = @"[A-Za-z]";
+            string digitsPattern = @"\d";
+            string command = "";
+
+            while ((command = Console.ReadLine()) != "end of race")
+            {
+                StringBuilder currentName = new StringBuilder();
+                MatchCollection lettersMatches = Regex.Matches(command, lettersPattern);
+                foreach (Match match in lettersMatches)
+                {
+                    currentName.Append(match.Value);
+                }
+
+
+                uint currentDistance = 0;
+                MatchCollection digitsMatches = Regex.Matches(command, digitsPattern);
+                foreach (Match match in digitsMatches)
+                {
+                    currentDistance += uint.Parse(match.Value);
+                }
+
+                Participant foundParticipant = participants.FirstOrDefault(p => p.Name == currentName.ToString());
+                if (foundParticipant != null)
+                {
+                    foundParticipant.Distance += currentDistance;
+                }
+
+            }
+
+            List<Participant> firstThreeParticipants = participants.OrderByDescending(p => p.Distance).Take(3).ToList();
+            Console.WriteLine($"1st place: {firstThreeParticipants[0].Name}");
+            Console.WriteLine($"2nd place: {firstThreeParticipants[1].Name}");
+            Console.WriteLine($"3rd place: {firstThreeParticipants[2].Name}");
+
+        }
+    }
+
+    class Participant
+    {
+        public Participant (string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public uint Distance { get; set; }
+    }
+}
---
+++ Fundamentals - September 2023/RegularExpressionsExercise/02. Race/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lettersPattern = @"[A-Za-z]+";
+            string digitsPattern = @"\d+";
+            string command = "";
+
+            string[] validNames = Console.ReadLine().Split(", ");
+
+            Regex regexLetters = new Regex(lettersPattern);
+            Regex regexDigits = new Regex(digitsPattern);
+            List<string> names = new List<string>();
+
+            Dictionary<string, int> participants = new Dictionary<string, int>();
+
+            while ((command = Console.ReadLine()) != "end of race")
+            {
+                Match participantName = regexLetters.Match(command);
+                Match participantDigits = regexDigits.Match(command);
+
+                int currentDistance = 0;
+
+                if (participantName.Success && participantDigits.Success)
+                {
+                    names.Add(participantName.Value);
+                    string[] digits = participantDigits.Value.Split();
+                    foreach (string digit in digits)
+                    {
+                        int currentDigit = int.Parse(digit);
+                        currentDistance += currentDigit;
+                    }
+                }
+
+            }
+        }
+    }
+}
