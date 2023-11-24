daae64d4ded3b4debb6bc9c96bd6dfc883bc8ca9
Bozhidar1804 <bobo1804@abv.bg>
2023-11-24 17:09:49 +0200

    Solved Ex2 from Exam Preparation

---
+++ Fundamentals - September 2023/ExamPreparation/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string pattern = @"@#+[A-Z]{1}[A-Za-z0-9]{4,}[A-Z]{1}@#+";
+            Regex regex = new Regex(pattern);
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    char[] inputArr = input.ToCharArray();
+                    string concatenation = "";
+                    for (int j = 0; j < inputArr.Length; j++)
+                    {
+                        if (char.IsDigit(inputArr[j]))
+                        {
+                            concatenation += inputArr[j];
+                        }
+                    }
+
+                    if (concatenation.Length == 0)
+                    {
+                        Console.WriteLine($"Product group: 00");
+                    } else
+                    {
+                        Console.WriteLine($"Product group: {concatenation}");
+                    }
+                } else 
+                {
+                    Console.WriteLine("Invalid barcode");
+                    continue;
+                }
+            }
+        }
+    }
+}
