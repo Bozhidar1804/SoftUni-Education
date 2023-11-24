65d3cd91e5a95007b922b807f81664258c12bfdd
Bozhidar1804 <bobo1804@abv.bg>
2023-11-24 16:35:54 +0200

    Solved Ex1 From ExamPreparation

---
+++ Fundamentals - September 2023/ExamPreparation/01. Secret Chat/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _01._Secret_Chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string command = "";
+
+            while ((command = Console.ReadLine()) != "Reveal")
+            {
+                string[] commandArr = command.Split(":|:").ToArray();
+                string currentCommand = commandArr[0];
+
+                if (currentCommand == "InsertSpace")
+                {
+                    int index = int.Parse(commandArr[1]);
+                    input = input.Insert(index, " ");
+                } else if (currentCommand == "ChangeAll")
+                {
+                    string substring = commandArr[1];
+                    string replacement = commandArr[2];
+                    input = input.Replace(substring, replacement);
+                } else if (currentCommand == "Reverse")
+                {
+                    string substring = commandArr[1];
+                    int substringIndex = input.IndexOf(substring);
+                    if (substringIndex < 0)
+                    {
+                        Console.WriteLine("error");
+                        continue;
+                    } else
+                    {
+                        input = input.Remove(substringIndex, substring.Length);
+                        string reversedSubstring = new string(substring.Reverse().ToArray());
+                        input = input.Insert(substringIndex, reversedSubstring);
+                    }
+                }
+                Console.WriteLine(input);
+            }
+            Console.WriteLine($"You have a new text message: {input}");
+        }
+    }
+}
---
+++ Fundamentals - September 2023/ListsExercise/test/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace List_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "End")
+                {
+                    break;
+                }
+
+
+                string[] partsCommand = command.Split();
+
+                string firstElementCommand = partsCommand[0];
+
+
+                if (firstElementCommand == "Add")
+                {
+                    int number = int.Parse(partsCommand[1]);
+                    numbers.Add(number);
+                }
+
+                else if (firstElementCommand == "Insert")
+                {
+                    int number = int.Parse(partsCommand[1]);
+                    int index = int.Parse(partsCommand[2]);
+
+                    if (!IsValid(index, numbers))
+                    {
+                        Console.WriteLine("Invalid index");
+                        continue;
+                    }
+
+                    numbers.Insert(index, number);
+                }
+
+                else if (firstElementCommand == "Remove")
+                {
+                    int index = int.Parse(partsCommand[1]);
+
+                    if (!IsValid(index, numbers))
+                    {
+                        Console.WriteLine("Invalid index");
+                        continue;
+                    }
+
+                    numbers.RemoveAt(index);
+                }
+
+                else if (firstElementCommand == "Shift")
+                {
+                    string secondElementCommand = partsCommand[1];
+                    int count = int.Parse(partsCommand[2]);
+                    {
+                        if (secondElementCommand == "left")
+                        {
+                            for (int i = 0; i < count; i++)
+                            {
+                                int firstNumberInNummbers = numbers[0];
+                                numbers.RemoveAt(0);
+                                numbers.Add(firstNumberInNummbers);
+                            }
+                        }
+
+                        else
+                        {
+                            for (int i = 0; i < count; i++)
+                            {
+                                int lastNumberInNumbers = numbers[numbers.Count - 1];
+                                numbers.RemoveAt(numbers.Count - 1);
+                                numbers.Insert(0, lastNumberInNumbers);
+                            }
+                        }
+                    }
+                }
+
+
+
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static bool IsValid(int index, List<int> numbers)
+        {
+            return index >= 0 && index < numbers.Count;
+        }
+    }
+}
