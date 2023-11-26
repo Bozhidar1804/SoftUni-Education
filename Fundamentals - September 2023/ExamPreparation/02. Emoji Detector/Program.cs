871e3c91c340a3559dd0168166c169d0cdf82479
Bozhidar1804 <bobo1804@abv.bg>
2023-11-26 22:21:57 +0200

    Solved Second Ex2 from ExamPreparation

---
+++ Fundamentals - September 2023/ExamPreparation/02. Emoji Detector/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Emoji_Detector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string validEmojiPattern = @"(\*{2}|\:{2})(?<Emoji>[A-Z][a-z]{2,})\1";
+            string digitsPattern = @"\d";
+            string input = Console.ReadLine();
+            
+            Regex regexEmojis = new Regex(validEmojiPattern);
+            MatchCollection emojiMatches = regexEmojis.Matches(input);
+            Regex regexDigits = new Regex(digitsPattern);
+            MatchCollection digitsMatches = regexDigits.Matches(input);
+
+            ulong coolSum = 1;
+
+            foreach (Match match in digitsMatches)
+            {
+                coolSum *= ulong.Parse(match.Value);
+            }
+
+            Console.WriteLine($"Cool threshold: {coolSum}");
+
+            List<string> coolEmojis = new List<string>();
+            int validEmojisCount = 0;
+            foreach (Match match in emojiMatches)
+            {
+                string emojiName = match.Groups["Emoji"].Value;
+                ulong valuesSum = 0;
+                foreach (char c in emojiName)
+                {
+                    valuesSum += c;
+                }
+
+                if (valuesSum > coolSum)
+                {
+                    coolEmojis.Add(match.ToString());
+                }
+                validEmojisCount++;
+            }
+
+            Console.WriteLine($"{validEmojisCount} emojis found in the text. The cool ones are:");
+            foreach (string coolEmoji in coolEmojis)
+            {
+                Console.WriteLine($"{coolEmoji}");
+            }
+        }
+    }
+}
