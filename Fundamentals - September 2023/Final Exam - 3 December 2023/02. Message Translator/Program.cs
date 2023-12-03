ff55abd4492821c740bfdc65db4e0b1d4fe6ae1f
Bozhidar1804 <bobo1804@abv.bg>
2023-12-03 14:13:57 +0200

    Solved the last Problem (2), total time: 1 hour and 30 minutes

---
+++ Fundamentals - September 2023/Final Exam - 3 December 2023/02. Message Translator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Message_Translator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string pattern = @"!(?<command>[A-Z]{1}[a-z]{2,})!:\[(?<text>[A-z]{8,})\]";
+            Regex regex = new Regex(pattern);
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                if (regex.IsMatch(input, 0))
+                {
+                    Match match = regex.Match(input);
+                    string command = match.Groups["command"].Value;
+                    string text = match.Groups["text"].Value;
+
+                    Console.Write($"{command}: ");
+
+                    foreach (char c in text)
+                    {
+                        int number = (int)c;
+                        Console.Write($"{number} ");
+                    }
+                    Console.WriteLine();
+                } else
+                {
+                    Console.WriteLine("The message is invalid");
+                }
+            }
+        }
+    }
+}
