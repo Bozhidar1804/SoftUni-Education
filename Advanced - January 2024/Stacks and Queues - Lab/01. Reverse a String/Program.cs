b3be54974defa4ad47b55483dba442e845d220e3
Bozhidar1804 <bobo1804@abv.bg>
2024-01-09 23:00:28 +0200

    Created Advanced folder and solved the first ex for this module

---
+++ Advanced - January 2024/Stacks and Queues - Lab/01. Reverse a String/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01._Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] inputArr = input.ToCharArray(); 
+
+            Stack<char> letters = new Stack<char>();
+
+            foreach (char c in inputArr)
+            {
+                letters.Push(c);
+            }
+
+            while (letters.Count != 0)
+            {
+                Console.Write(letters.Pop());
+            }
+        }
+    }
+}
---
+++ Fundamentals - September 2023/Final Exam - 3 December 2023/Theoretical Part Tests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Theoretical_Part_Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            printText("C#");
+        }
+        public static void printText(string text)
+        {
+            Console.WriteLine("I love" + text);
+        }
+    }
+}
