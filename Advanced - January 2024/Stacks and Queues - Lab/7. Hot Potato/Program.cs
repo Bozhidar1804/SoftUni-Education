4e972a1b320bdff34fb8b9da1239923a82f411e5
Bozhidar1804 <bobo1804@abv.bg>
2024-01-11 21:55:10 +0200

    Solved Ex7 from StacksAndQueuesLab

---
+++ Advanced - January 2024/Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> people = new Queue<string>(Console.ReadLine().Split());
+            Queue<string> backupPeople = new Queue<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            while (people.Count != 1)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i == n)
+                    {
+                        Console.WriteLine($"Removed {people.Dequeue()}");
+                    } else if (n > 1)
+                    {
+                        string currentRemoved = people.Dequeue();
+                        people.Enqueue(currentRemoved);
+                    } else
+                    {
+
+                    }
+                }
+            }
+
+            Console.WriteLine($"Last is {people.Dequeue()}");
+        }
+    }
+}
