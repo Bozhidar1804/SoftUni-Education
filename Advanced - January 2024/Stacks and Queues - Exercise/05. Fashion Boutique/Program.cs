df1cc969ca261dd8ade8d4e585eb1299a09c5251
Bozhidar1804 <bobo1804@abv.bg>
2024-01-14 00:46:47 +0200

    Solved Ex5 from StacksAndQueuesEx

---
+++ Advanced - January 2024/Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothes = new Stack<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+            int capacity = int.Parse(Console.ReadLine());
+            int currentRackCapacity = capacity;
+            int sum = 0;
+            int racks = 0;
+
+            if (clothes.Any())
+            {
+                racks++;
+            }
+
+            while (clothes.Any())
+            {
+                if (clothes.Peek() <= currentRackCapacity)
+                {
+                    currentRackCapacity -= clothes.Pop();
+                } else
+                {
+                    racks++;
+                    currentRackCapacity = capacity;
+                }
+            }
+
+            Console.WriteLine(racks);
+        }
+    }
+}
