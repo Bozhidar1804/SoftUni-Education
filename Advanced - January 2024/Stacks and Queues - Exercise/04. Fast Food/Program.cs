2c296a2f1a2bbd1048cb78bdeec000ecf8542bcc
Bozhidar1804 <bobo1804@abv.bg>
2024-01-13 12:53:50 +0200

    Solved Ex4 from StacksAndQueuesEx

---
+++ Advanced - January 2024/Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalFood = int.Parse(Console.ReadLine());
+
+            Queue<int> orders = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Count >= 0)
+            {
+                if (orders.Count == 0)
+                {
+                    Console.WriteLine("Orders complete");
+                    break;
+                }
+
+                int currentOrder = orders.Peek();
+                if (totalFood - currentOrder > 0 || currentOrder == totalFood)
+                {
+                    orders.Dequeue();
+                    totalFood -= currentOrder;
+                } else if (currentOrder > totalFood)
+                {
+                    break;
+                }
+            }
+
+            if (orders.Count > 0)
+            {
+                string result = "";
+                while (orders.Count > 0)
+                {
+                    result += orders.Dequeue() + " ";
+                }
+                Console.WriteLine($"Orders left: {result}");
+            }
+        }
+    }
+}
