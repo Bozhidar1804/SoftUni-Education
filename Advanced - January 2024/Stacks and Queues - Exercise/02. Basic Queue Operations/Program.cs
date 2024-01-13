85a21b9ff979bc8408a129ba1cf3bc4752dbb2a1
Bozhidar1804 <bobo1804@abv.bg>
2024-01-13 12:15:43 +0200

    Solved Ex2 from StacksAndQueuesEx

---
+++ Advanced - January 2024/Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            Queue<int> q = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+
+            for (int i = 0; i < s; i++)
+            {
+                q.Dequeue();
+            }
+            
+            if (q.Contains(x))
+            {
+                Console.WriteLine("true");
+            } else if (q.Count == 0)
+            {
+                Console.WriteLine(0);
+            } else
+            {
+                Console.WriteLine(q.Min());
+            }
+        }
+    }
+}
