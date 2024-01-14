6053d05e6159a8ab1bc9252acc7d78005907e751
Bozhidar1804 <bobo1804@abv.bg>
2024-01-14 23:48:38 +0200

    Solved Ex1 from MultiDArrLab

---
+++ Advanced - January 2024/Multidimensional Arrays - Lab/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _1._Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+
+            int sum = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] currentRow = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                foreach (int number in currentRow)
+                {
+                    sum += number;
+                }
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Advanced - January 2024/Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _09._Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<char> text = new Stack<char>();
+            string lastString = "";
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(" ");
+                int number = int.Parse(command[0]);
+
+                if (number == 1)
+                {
+                    lastString = new string(text.ToString());
+
+                    char[] addedText = command[1].ToCharArray();
+                    Array.Reverse(addedText);
+                    foreach (char c in addedText)
+                    {
+                        text.Push(c);
+                    }
+                } else if (number == 2)
+                {
+                    lastString = new string(text.ToString());
+
+                    int count = int.Parse(command[1]);
+                    for (int j = 0; j < count; j++)
+                    {
+                        text.Pop();
+                    }
+                } else if (number == 3)
+                {
+                    int index = int.Parse(command[1]);
+                    if (index > 0)
+                    {
+                        index--;
+                    }
+                    string currentText = "";
+                    foreach (char c in text)
+                    {
+                        currentText += c;
+                    }
+                    char[] currentTextCharArr = currentText.ToCharArray();
+                    Console.WriteLine(currentTextCharArr[index]);
+                } else if (number == 4)
+                {
+                    char[] lastTextArr = lastString.ToCharArray();
+                    Array.Reverse(lastTextArr);
+                    Stack<char> lastText = new Stack<char>();
+                    foreach (char c in lastTextArr)
+                    {
+                        lastText.Push(c);
+                    }
+
+                    text = lastText;
+                }
+            }
+        }
+    }
+}
