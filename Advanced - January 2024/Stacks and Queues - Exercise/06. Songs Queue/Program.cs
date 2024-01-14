c1e9b5999a11e8a93927df0589d02508645659aa
Bozhidar1804 <bobo1804@abv.bg>
2024-01-14 19:56:35 +0200

    Solved Ex6 from StackAndQueuesEx

---
+++ Advanced - January 2024/Stacks and Queues - Exercise/06. Songs Queue/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _06._Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songs = new Queue<string>(Console.ReadLine().Split(", "));
+
+            while (songs.Count >= 0)
+            {
+                string command = Console.ReadLine();
+                if (songs.Count == 0)
+                {
+                    break;
+                }
+
+                if (command == "Play")
+                {
+                    songs.Dequeue();
+                } else if (command.Contains("Add"))
+                {
+                    string song = command.Substring(4);
+                    if (songs.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    } else
+                    {
+                        songs.Enqueue(song);
+                    }
+                } else if (command == "Show")
+                {
+                    int songsCount = songs.Count;
+                    foreach (string song in songs)
+                    {
+                        if (songsCount == 1)
+                        {
+                            Console.Write($"{song}");
+                            Console.WriteLine();
+                            break;
+                        }
+                        Console.Write($"{song}, ");
+                        songsCount--;
+                    }
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
