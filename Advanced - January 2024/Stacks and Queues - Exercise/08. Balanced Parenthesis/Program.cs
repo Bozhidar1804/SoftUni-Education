9a1b2781365f272c60fd3d6181760af33f7ab55b
Bozhidar1804 <bobo1804@abv.bg>
2024-01-14 21:21:56 +0200

    Solved Ex8 from StackAndQueuesEx

---
+++ Advanced - January 2024/Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] sequence = Console.ReadLine().ToCharArray();
+
+            if (sequence.Length % 2 != 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            Stack<char> charStack = new Stack<char>();
+
+            foreach (char c in sequence)
+            {
+                if ("{[(".Contains(c))
+                {
+                    charStack.Push(c);
+                } else if (c == ')' && charStack.Peek() == '(')
+                {
+                    charStack.Pop();
+                } else if (c == ']' && charStack.Peek() == '[')
+                {
+                    charStack.Pop();
+                } else if (c == '}' && charStack.Peek() == '{')
+                {
+                    charStack.Pop();
+                }
+            }
+
+            Console.WriteLine(charStack.Any() ? "NO" : "YES");
+        }
+    }
+}
