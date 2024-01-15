080f9d91c0a6209849192e72a445c3cf65e35e06
Bozhidar1804 <bobo1804@abv.bg>
2024-01-15 22:30:59 +0200

    Solved Ex2 and Ex3 from MultiDArrLab

--- Advanced - January 2024/Multidimensional Arrays - Lab/2. Sum Matrix Columns/Program.cs
+++ Advanced - January 2024/Multidimensional Arrays - Lab/2. Sum Matrix Columns/Program.cs
@@ -9,9 +9,23 @@
             int cols = input[1];
             int[,] matrix = new int[rows, cols];
 
-            for (int i = 0; i < rows; i++)
+            for (int row = 0; row < rows; row++)
             {
-                int[] currentRow = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                int[] currentRow = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
             }
         }
     }
---
+++ Advanced - January 2024/Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _3._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int rows = n;
+            int cols = n;
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currentRow = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row,col] = currentRow[col];
+                }
+            }
+            int sum = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (row == col)
+                    {
+                        sum += matrix[row,col];
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
