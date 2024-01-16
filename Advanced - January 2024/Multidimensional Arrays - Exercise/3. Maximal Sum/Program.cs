04bd23175d838f4ec7286c6623a201b11beb003a
Bozhidar1804 <bobo1804@abv.bg>
2024-01-16 23:25:04 +0200

    60/100 (solved) ex3 from MultiDArrEx

---
+++ Advanced - January 2024/Multidimensional Arrays - Exercise/3. Maximal Sum/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _3._Maximal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+            int[][] matrix = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                matrix[row] = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            }
+
+            int maxSum = int.MinValue;
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int row = 0; row < rows - 2; row++)
+            {
+                for (int col = 0; col < cols - 2; col++)
+                {
+                    int sum = 0;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            sum += matrix[row + i][col + j];
+                        }
+                    }
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+            for (int row = maxRow; row < maxRow + 3; row++)
+            {
+                for (int col = maxCol; col < maxCol + 3; col++)
+                {
+                    Console.Write($"{matrix[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
