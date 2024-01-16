09a05b0805fdd11e9c535b00c38f1c6c068ff266
Bozhidar1804 <bobo1804@abv.bg>
2024-01-16 22:05:56 +0200

    Solved Ex1 from MultiDArrEx

---
+++ Advanced - January 2024/Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _1._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int rows = n;
+            int cols = n;
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currentRow = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int sumLeftToRight = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (row == col)
+                    {
+                        sumLeftToRight += matrix[row, col];
+                    }
+                }
+            }
+
+            int sumRightToLeft = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = cols - 1; col >= 0; col--)
+                {
+                    if (col == n - 1 - row)
+                    {
+                        sumRightToLeft += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(sumLeftToRight - sumRightToLeft));
+        }
+    }
+}
