f001558a2e29c94358d2e08ffc2eaf82a811528e
Bozhidar1804 <bobo1804@abv.bg>
2024-01-17 23:35:48 +0200

    Solved Ex5 from MultiDArrEx

---
+++ Advanced - January 2024/Multidimensional Arrays - Exercise/5. Snake Moves/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _5._Snake_Moves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+
+            string[,] matrix = new string[rows, cols];
+
+            string snake = Console.ReadLine();
+            int snakeCounter = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        matrix[row, col] = snake[snakeCounter].ToString();
+                        snakeCounter = GetSnakeValue(snake, snakeCounter);
+                    }
+                } else if (row % 2 != 0)
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = snake[snakeCounter].ToString();
+                        snakeCounter = GetSnakeValue(snake, snakeCounter);
+                    }
+                } 
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write($"{matrix[row, col]}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int GetSnakeValue(string snake, int snakeCounter)
+        {
+            snakeCounter++;
+            if (snakeCounter >= snake.Length)
+            {
+                snakeCounter = 0;
+            }
+            return snakeCounter;
+        }
+    }
+}
