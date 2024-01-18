ad0b99c472029afb1bd6d61ccf026202aadb97f2
Bozhidar1804 <bobo1804@abv.bg>
2024-01-18 18:34:28 +0200

    Solved Ex1 from SetsAndDicsAdvancedLab

---
+++ Advanced - January 2024/Multidimensional Arrays - Exercise/7. Knight Game/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _7._Knight_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int rows = n;
+            int cols = n;
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] currentRow = Console.ReadLine().Split();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int removedKnightsCounter = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (matrix[row, col] == "K")
+                    {
+                        if (matrix[])
+                    }
+                    else { continue; }
+                }
+            }
+        }
+    }
+}
---
+++ Advanced - January 2024/Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+            Dictionary<double, int> times = new Dictionary<double, int>();
+
+            foreach (double number in input)
+            {
+                if (!times.ContainsKey(number))
+                {
+                    times[number] = 0;
+                }
+                times[number]++;
+            }
+
+            foreach (var number in times)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+
+        }
+    }
+}
