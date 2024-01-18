57ae427645a74ff69af098e5a485be08cfa125cf
Bozhidar1804 <bobo1804@abv.bg>
2024-01-18 21:48:12 +0200

    solved Ex3 from SetsAndDicsAdvancedLab

---
+++ Advanced - January 2024/Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int[] sorted = input.OrderByDescending(n => n).ToArray();
+
+            if (sorted.Length < 3)
+            {
+                for (int i = 0; i < sorted.Length; i++)
+                {
+                    Console.Write($"{sorted[i]} ");
+                }
+            } else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write($"{sorted[i]} ");
+                }
+            }
+        }
+    }
+}
