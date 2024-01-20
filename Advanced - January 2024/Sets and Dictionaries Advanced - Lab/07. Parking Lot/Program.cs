1e1f26be13181b74c419c2bde460647855796b50
Bozhidar1804 <bobo1804@abv.bg>
2024-01-20 13:08:35 +0200

    Solved Ex7 from SetsAndDicsAdvLab

---
+++ Advanced - January 2024/Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+            string command = "";
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] commandArr = command.Split(", ");
+                string direction = commandArr[0];
+                string number = commandArr[1];
+
+                if (direction == "IN")
+                {
+                    cars.Add(number);
+                } else if (direction == "OUT")
+                {
+                    cars.Remove(number);
+                }
+            }
+
+            if (cars.Count == 0)
+            {
+                Console.WriteLine($"Parking Lot is Empty");
+            } else if (cars.Count > 0)
+            {
+                foreach (string number in cars)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
