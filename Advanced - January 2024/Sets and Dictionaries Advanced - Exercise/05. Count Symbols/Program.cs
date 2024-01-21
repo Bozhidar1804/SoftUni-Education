195c4cdcff0831a0829e1d19f5df8c4177bd5204
Bozhidar1804 <bobo1804@abv.bg>
2024-01-21 15:25:09 +0200

    Solved Ex5 from SetsAndDicsAdvEx

---
+++ Advanced - January 2024/Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] inputArr = Console.ReadLine().ToCharArray();
+            SortedDictionary<char, int> occurrences = new SortedDictionary<char, int>();
+
+            foreach (char c in inputArr)
+            {
+                if (!occurrences.ContainsKey(c))
+                {
+                    occurrences[c] = 1;
+                }
+                else if (occurrences.ContainsKey(c))
+                {
+                    occurrences[c]++;
+                }
+            }
+
+            foreach (KeyValuePair<char, int> kvp in occurrences)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
