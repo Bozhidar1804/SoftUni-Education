b7bc0abb19ad3c87ca4620a50dcb8359892d7c4f
Bozhidar1804 <bobo1804@abv.bg>
2024-01-21 15:12:17 +0200

    Solved Ex4 from SetsAndDicsAdvEx

---
+++ Advanced - January 2024/Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int, int> occurrences = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (!occurrences.ContainsKey(input))
+                {
+                    occurrences[input] = 1;
+                } else if (occurrences.ContainsKey(input))
+                {
+                    occurrences[input]++;
+                }
+            }
+            int evenTimesNumber = 0;
+            foreach (KeyValuePair<int, int> kvp in occurrences)
+            {
+                if (kvp.Value % 2 == 0)
+                {
+                    evenTimesNumber = kvp.Key;
+                }
+            }
+            Console.WriteLine(evenTimesNumber);
+        }
+    }
+}
