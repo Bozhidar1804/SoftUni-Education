67a9c8e04c36bcdeba587487d3d15e6bdf821e05
Bozhidar1804 <bobo1804@abv.bg>
2024-01-21 15:01:20 +0200

    Solved Ex3 from SetsAndDicsAdvEx

---
+++ Advanced - January 2024/Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> periodicTable = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputArr = Console.ReadLine().Split(" ");
+                foreach (string element in inputArr)
+                {
+                    periodicTable.Add(element);
+                }
+            }
+
+            periodicTable = periodicTable.OrderBy(e => e).ToHashSet();
+            foreach (string element in periodicTable)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
