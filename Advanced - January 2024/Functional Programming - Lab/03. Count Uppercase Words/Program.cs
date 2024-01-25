03760c3c1cd4d32e574d16469f56cd42e63a6d09
Bozhidar1804 <bobo1804@abv.bg>
2024-01-25 18:16:33 +0200

    Solved Ex3 FuncProgLab

---
+++ Advanced - January 2024/Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Predicate<string> checker = word => char.IsUpper(word[0]);
+
+            words = words.Where(w => checker(w)).ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
