8cf7571bf158d48758b7f0d1f58ec60309fe18cd
Bozhidar1804 <bobo1804@abv.bg>
2024-01-25 18:02:16 +0200

    Solved Ex2 from FuncProgLab

---
+++ Advanced - January 2024/Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Func<string, int> parser = n => int.Parse(n);
+            int[] numbers = input.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(parser).ToArray();
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
