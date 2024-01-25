af6cc194541b7158fcf828ca31307b3d0c949ec8
Bozhidar1804 <bobo1804@abv.bg>
2024-01-26 00:11:54 +0200

    Solved Ex4 from FuncProgLab

---
+++ Advanced - January 2024/Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(n => n * 1.2)
+                .ToArray();
+
+            foreach (double number in numbers)
+            {
+                Console.WriteLine($"{number:F2}");
+            }
+
+
+        }
+    }
+}
