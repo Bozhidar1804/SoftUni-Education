77f761135d403410f839ebe124f0003418e41283
Bozhidar1804 <bobo1804@abv.bg>
2024-01-27 15:05:54 +0200

    Solved Ex3 from FuncProgLab

---
+++ Advanced - January 2024/Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> input = Console.ReadLine().Split(" ").Select(int.Parse).ToHashSet();
+
+            Func<HashSet<int>, int> PrintMin = input =>
+            {
+                int min = int.MaxValue;
+
+                foreach (int n in input)
+                {
+                    if (n < min)
+                    {
+                        min = n;
+                    }
+                }
+
+                return min;
+            };
+
+            Console.WriteLine(PrintMin(input));
+        }
+    }
+}
