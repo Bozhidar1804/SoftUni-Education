22dee9b9adf42dbd92c1d81fae4c25b93718a271
Bozhidar1804 <bobo1804@abv.bg>
2024-01-27 14:47:25 +0200

    Solved Ex1 from FuncProgEx

---
+++ Advanced - January 2024/Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            Action<string> print = n => Console.WriteLine(n);
+
+            Array.ForEach(input, print);
+        }
+    }
+}
