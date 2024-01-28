a19a81ea14430d9fda817b553dfa983094b47dac
Bozhidar1804 <bobo1804@abv.bg>
2024-01-28 21:07:34 +0200

    Solved Ex10 from FuncProgEx

---
+++ Advanced - January 2024/Functional Programming - Exercise/10. Party Reservation Filter Module/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _10._Party_Reservation_Filter_Module
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> people = Console.ReadLine().Split(" ").ToList();
+            Dictionary<string, Predicate<string>> filters = new Dictionary<string, Predicate<string>>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Print")
+            {
+                string[] commandArr = command.Split(";");
+                string action = commandArr[0];
+                string filter = commandArr[1];
+                string value = commandArr[2];
+
+                if (action == "Add filter")
+                {
+                    if (!filters.ContainsKey(filter + value))
+                    {
+                        filters.Add(filter + value, GetPredicate(filter, value));
+                    }
+                } else
+                {
+                    filters.Remove(filter + value);
+                }
+            }
+
+            foreach (var filter in filters)
+            {
+                people.RemoveAll(filter.Value);
+            }
+
+            if (people.Any())
+            {
+                Console.WriteLine($"{string.Join(" ", people)}");
+            }
+        }
+
+        static Predicate<string> GetPredicate(string filter, string value)
+        {
+            switch (filter)
+            {
+                case "Starts with":
+                    return p => p.StartsWith(value);
+                case "Ends with":
+                    return p => p.EndsWith(value);
+                case "Length":
+                    return p => p.Length == int.Parse(value);
+                case "Contains":
+                    return p => p.Contains(value);
+                default:
+                    return default;
+            }
+        }
+    }
+}
