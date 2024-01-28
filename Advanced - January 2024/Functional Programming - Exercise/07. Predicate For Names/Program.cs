c84e4dff70078db31cf68b2065a1d58be2ab8324
Bozhidar1804 <bobo1804@abv.bg>
2024-01-28 17:49:59 +0200

    Solved Ex7 and Ex8 from FuncProgEx

---
+++ Advanced - January 2024/Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<List<string>, Predicate<string>> FilterNames = (names, match) =>
+            {
+                foreach (string name in names)
+                {
+                    if (match(name))
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            };
+            
+            int length = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split(" ").ToList();
+
+            FilterNames(names, n => n.Length <= length);
+        }
+    }
+}
---
+++ Advanced - January 2024/Functional Programming - Exercise/08. List Of Predicates/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace _08._List_Of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, List<int>, List<int>> FindNumbers = (n, dividers) =>
+            {
+                List<int> divisibleNumbers = new List<int>();
+
+                for (int i = 1; i <= n; i++)
+                {
+                    bool isDivisible = false;
+
+                    foreach (int divider in dividers)
+                    {
+                        Predicate<int> match = i => i % divider == 0;
+
+                        if (match(i))
+                        {
+                            isDivisible = true;
+                        } else
+                        {
+                            isDivisible = false;
+                        }
+                    }
+
+                    if (isDivisible)
+                    {
+                        divisibleNumbers.Add(i);
+                    }
+                }
+
+                return divisibleNumbers;
+            };
+
+            int n = int.Parse(Console.ReadLine());
+            List<int> dividers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            List<int> result = FindNumbers(n, dividers);
+            foreach (int num in result)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
