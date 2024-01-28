3e6bea4883ddf05cf9d32eab4b2ba8941a191a6b
Bozhidar1804 <bobo1804@abv.bg>
2024-01-28 13:22:24 +0200

    Solved Ex5 from FuncProgLab

---
+++ Advanced - January 2024/Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _05._Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, List<int>, List<int>> calculate = (command, numbers) =>
+            {
+                List<int> result = new List<int>();
+
+                foreach (int number in numbers)
+                {
+                    switch (command)
+                    {
+                        case "add":
+                            result.Add(number + 1);
+                            break;
+                        case "multiply":
+                            result.Add(number * 2);
+                            break;
+                        case "subtract":
+                            result.Add(number - 1);
+                            break;
+                        default: break;
+                    }
+                }
+
+                return result;
+            };
+            Action<List<int>> print = numbers => Console.WriteLine(string.Join(" ", numbers));
+
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            string command = "";
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "print")
+                {
+                    print(numbers);
+                }
+                else
+                {
+                    numbers = calculate(command, numbers);
+                }
+            }
+        }
+    }
+}
