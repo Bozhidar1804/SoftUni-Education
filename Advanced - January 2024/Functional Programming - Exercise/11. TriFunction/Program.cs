d56395d286d1a8bdabad7c520268adef01334491
Bozhidar1804 <bobo1804@abv.bg>
2024-01-28 21:32:03 +0200

    Solved Ex11 from FuncProgEx

---
+++ Advanced - January 2024/Functional Programming - Exercise/11. TriFunction/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml.Linq;
+
+namespace _11._TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> ValidNameCheck = (name, n) =>
+            {
+                bool result = false;
+                char[] nameArr = name.ToCharArray();
+                int sum = 0;
+                foreach (char c in nameArr)
+                {
+                    sum += c;
+                }
+
+                if (sum >= n)
+                {
+                    result = true;
+                }
+                return result;
+            };
+
+            Func<Func<string, int, bool>, string[], int, string> GetValidName = (ValidNameCheck, names, n) =>
+            {
+                string output = "";
+
+                foreach(string name in names)
+                {
+                    if (ValidNameCheck(name, n))
+                    {
+                        output = name;
+                        break;
+                    }
+                }
+
+                return output;
+            };
+
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ");
+            Console.WriteLine(GetValidName(ValidNameCheck, names, n));
+            
+        }
+    }
+}
