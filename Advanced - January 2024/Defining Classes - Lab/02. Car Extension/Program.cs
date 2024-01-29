d17cd6956c3f2a4dceaf2a2bbd69cd1364b19a9d
Bozhidar1804 <bobo1804@abv.bg>
2024-01-30 00:41:37 +0200

    Solved Ex2 from DefiningClassesLab

---
+++ Advanced - January 2024/Defining Classes - Lab/02. Car Extension/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "BMW";
+            car.Model = "320";
+            car.Year = 2000;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            car.Drive(2000);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
