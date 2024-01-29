7f487d0426802598117fe569fa8f928788934bf1
Bozhidar1804 <bobo1804@abv.bg>
2024-01-30 00:57:16 +0200

    Solved Ex3 from Defining Classes Lab

---
+++ Advanced - January 2024/Defining Classes - Lab/03. Car Constructors/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public Car ()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+
+        public Car (string make, string model, int year)
+            : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public string Make { get { return this.make; } set { this.make = value; } }
+        public string Model { get { return this.model; } set { this.model = value; } }
+        public int Year { get { return this.year; } set { this.year = value; } }
+        public double FuelQuantity { get { return this.fuelQuantity; } set { this.fuelQuantity = value; } }
+        public double FuelConsumption { get { return this.fuelConsumption; } set { this.fuelConsumption = value; } }
+    }
+}
---
+++ Advanced - January 2024/Defining Classes - Lab/03. Car Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            Car car2 = new Car("BMW", "320", 2000);
+            Car car3 = new Car("Mercedes", "G", 2016, 200, 200);
+
+            Console.WriteLine($"{car1.Make}, {car1.Model}, {car1.Year}, {car1.FuelQuantity}, {car1.FuelQuantity}");
+            Console.WriteLine($"{car2.Make}, {car2.Model}, {car2.Year}, {car2.FuelQuantity}");
+            Console.WriteLine($"{car3.Make}, {car3.Model}, {car3.Year}, {car3.FuelQuantity}, {car3.FuelQuantity}");
+        }
+    }
+}
