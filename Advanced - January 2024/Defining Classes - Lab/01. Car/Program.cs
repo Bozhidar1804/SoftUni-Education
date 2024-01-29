bcb8c8cf22a820092c45977d2ed9481f29703dde
Bozhidar1804 <bobo1804@abv.bg>
2024-01-29 22:16:34 +0200

    Solved Ex1 from DefiningClassesLab

---
+++ Advanced - January 2024/Defining Classes - Lab/01. Car/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "BMW";
+            car1.Model = "E46";
+            car1.Year = 1999;
+
+            Console.WriteLine($"Make: {car1.Make}, Model: {car1.Model}, Year: {car1.Year}");
+        }
+    }
+}
