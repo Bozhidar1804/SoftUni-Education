835104c9b89e9a37eff6c90084a08b04ef4f527e
Bozhidar1804 <bobo1804@abv.bg>
2024-01-31 21:51:03 +0200

    Solved Ex2 from DefiningClassesEx

---
+++ Advanced - January 2024/Defining Classes - Exercise/02. Creating Constructors/Program.cs
@@ -0,0 +1,14 @@
+﻿using DefiningClasses;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Person person1 = new Person(15);
+            Person person2 = new Person("Ivan", 21);
+        }
+    }
+}
