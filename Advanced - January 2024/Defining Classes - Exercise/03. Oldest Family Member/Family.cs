f9aef17caba4511d5fec248991410f468f17e567
Bozhidar1804 <bobo1804@abv.bg>
2024-02-01 01:27:16 +0200

    Solved Ex3 from DefiningClassesEx

---
+++ Advanced - January 2024/Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people = new List<Person>();
+
+        public void AddMember(Person person)
+        {
+            people.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            return people.FirstOrDefault(p => p.Age == people.Max(p1 => p1.Age));
+        }
+    }
+}
---
+++ Advanced - January 2024/Defining Classes - Exercise/03. Oldest Family Member/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person ()
+        {
+            name = "No name";
+            age = 1;
+        }
+
+        public Person (int age) : this()
+        {
+            this.age = age;
+        }
+
+        public Person (string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
