0ffcbfbffe5378dae0f68df8ddaa1cd39dfa0bd8
Bozhidar1804 <bobo1804@abv.bg>
2024-01-31 20:53:29 +0200

    Solved Ex1 from DefiningClassesEx

---
+++ Advanced - January 2024/Defining Classes - Exercise/01. Define a Class Person/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person ()
+        {
+
+        }
+        public Person (string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ Advanced - January 2024/Defining Classes - Exercise/01. Define a Class Person/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Bozhidar", 17);
+            Person person2 = new Person();
+            person2.Name = "Ivan";
+            person2.Age = 52;
+        }
+    }
+}
