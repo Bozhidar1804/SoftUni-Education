e3b7e211a30d9f6f4f1221f21f1732d29cc89e73
Bozhidar1804 <bobo1804@abv.bg>
2024-02-07 01:20:25 +0200

    Solved Ex1 from GenericsLab

---
+++ Advanced - January 2024/Generics - Lab/BoxofT/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> data = new List<T>();
+
+        public Box ()
+        {
+            data = new List<T> ();
+        }
+        public void Add(T element)
+        {
+            data.Add(element);
+        }
+
+        public T Remove()
+        {
+            var element = data.Last();
+            data.RemoveAt(data.Count - 1);
+            return element;
+        }
+
+        public int Count { get { return data.Count; } }
+
+        public List<T> Data { get; set; }
+    }
+}
