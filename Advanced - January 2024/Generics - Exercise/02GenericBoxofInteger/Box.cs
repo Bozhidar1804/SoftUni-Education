0e658b18e03e315035c869f12f84b228207a45df
Bozhidar1804 <bobo1804@abv.bg>
2024-02-07 12:15:28 +0200

    Solved Ex2 from GenericsEx

---
+++ Advanced - January 2024/Generics - Exercise/02GenericBoxofInteger/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02GenericBoxofInteger
+{
+    public class Box<T>
+    {
+        private List<T> list;
+
+        public Box ()
+        {
+            list = new List<T> ();
+        }
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (T element in list)
+            {
+                sb.AppendLine($"{typeof(T)}: {element}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Advanced - January 2024/Generics - Exercise/02GenericBoxofInteger/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _02GenericBoxofInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int item = int.Parse(Console.ReadLine());
+                box.Add(item);
+            }
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
