4d8c2299ac75385edda640575f232e11461c5e97
Bozhidar1804 <bobo1804@abv.bg>
2024-02-07 23:49:05 +0200

    Solved Ex4 from GenericsEx

---
+++ Advanced - January 2024/Generics - Exercise/04GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04GenericSwapMethodInteger
+{
+    public class Box<T>
+    {
+        public List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public void Swap(int index1, int index2)
+        {
+            (list[index1], list[index2]) = (list[index2], list[index1]);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (T element in list)
+            {
+                sb.AppendLine($"{typeof(T)}: {element}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Advanced - January 2024/Generics - Exercise/04GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _04GenericSwapMethodInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int item = int.Parse(Console.ReadLine());
+                box.Add(item);
+            }
+
+            int[] indexes = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            box.Swap(indexes[0], indexes[1]);
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
