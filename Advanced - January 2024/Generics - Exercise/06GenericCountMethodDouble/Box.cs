4a1f8864060d70bc47fce988eb4510f5ca086124
Bozhidar1804 <bobo1804@abv.bg>
2024-02-08 00:15:54 +0200

    Solved Ex6 from GenericsEx

---
+++ Advanced - January 2024/Generics - Exercise/06GenericCountMethodDouble/Box.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable
+    {
+        public List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public int Count(T element)
+        {
+            int count = 0;
+
+            foreach (T item in list)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Advanced - January 2024/Generics - Exercise/06GenericCountMethodDouble/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06GenericCountMethodDouble
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<double> box = new Box<double>();
+            for (int i = 0; i < n; i++)
+            {
+                double item = double.Parse(Console.ReadLine());
+                box.Add(item);
+            }
+            double compareItem = double.Parse(Console.ReadLine());
+            Console.WriteLine(box.Count(compareItem));
+        }
+    }
+}
