db010a37fc742272287d116d87996c1cf3cd2a86
Bozhidar1804 <bobo1804@abv.bg>
2024-02-07 23:40:27 +0200

    Solved Ex3 from GenericsEx

---
+++ Advanced - January 2024/Generics - Exercise/03GenericSwapMethodString/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03GenericSwapMethodString
+{
+    public class Box<T>
+    {
+        public List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+    }
+}
---
+++ Advanced - January 2024/Generics - Exercise/03GenericSwapMethodString/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03GenericSwapMethodString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string item = Console.ReadLine();
+                list.Add(item);
+            }
+
+            int[] indexes = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Swap(list, indexes[0], indexes[1]);
+
+            foreach (string item in list)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+
+        public static void Swap<T>(List<T> list, int index1, int index2)
+        {
+            (list[index1], list[index2]) = (list[index2], list[index1]);
+        }
+    }
+}
