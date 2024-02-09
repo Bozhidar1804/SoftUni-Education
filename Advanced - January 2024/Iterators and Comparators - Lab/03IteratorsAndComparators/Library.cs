a3ea2a912f693bf13a08e647b869f7509a5231cc
Bozhidar1804 <bobo1804@abv.bg>
2024-02-09 16:45:16 +0200

    Solved Ex3 from IteratorsAndComparatorsLab:

---
+++ Advanced - January 2024/Iterators and Comparators - Lab/03IteratorsAndComparators/Library.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] input)
+        {
+            books = new List<Book>(input);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books.OrderBy(b => b).ToList());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currentIndex;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public Book Current => books[currentIndex];
+            object IEnumerator.Current => Current;
+            public void Dispose()
+            {
+            }
+            public bool MoveNext()
+            {
+                return ++currentIndex < books.Count;
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+        }
+    }
+}
