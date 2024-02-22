a5a17d8f6edbe6691bd1d6d997c498ceabee4ef4
Bozhidar1804 <bobo1804@abv.bg>
2024-02-22 22:52:28 +0200

    Solved Ex5 from InheritanceLab

---
+++ Advanced - January 2024/C# OOP/Inheritance - Lab/05StackofStrings/StackOfStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
---
+++ Advanced - January 2024/C# OOP/Inheritance - Lab/05StackofStrings/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+
+            stack.AddRange(new string[] { "abc", "def" });
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine(stack.Count);
+        }
+    }
+}
