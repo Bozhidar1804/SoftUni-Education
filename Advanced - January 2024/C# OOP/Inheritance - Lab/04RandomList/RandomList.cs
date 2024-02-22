aad63be8a514e83f1d90c000a6b3143e6364debc
Bozhidar1804 <bobo1804@abv.bg>
2024-02-22 22:34:53 +0200

    Solved Ex4 from InheritanceLab

---
+++ Advanced - January 2024/C# OOP/Inheritance - Lab/04RandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+        public RandomList()
+        {
+            rnd = new Random();
+        }
+        public string RandomString(List<String> list)
+        {
+            int index = rnd.Next(list.Count);
+            string item = list[index];
+            list.Remove(item);
+            return item;
+        }
+    }
+}
---
+++ Advanced - January 2024/C# OOP/Inheritance - Lab/04RandomList/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("a");
+            list.Add("b");
+            list.Add("c");
+
+            RandomList strings = new RandomList();
+            Console.WriteLine(strings.RandomString(list));
+        }
+    }
+}
