7c5646c570c0fb68aa629d956b25f0599b68aed6
Bozhidar1804 <bobo1804@abv.bg>
2024-02-23 13:17:09 +0200

    Solved Ex1 from InheritanceEx

---
+++ Advanced - January 2024/C# OOP/Inheritance - Exercise/01Person/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person (string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Name: {Name}, Age: {Age}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Advanced - January 2024/C# OOP/Inheritance - Exercise/01Person/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01Person
+{
+    public class Program
+    {
+        static void Main()
+
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Child child = new Child(name, age);
+            Console.WriteLine(child);
+
+        }
+    }
+}
