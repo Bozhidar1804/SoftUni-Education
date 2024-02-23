b73001394f043e3e856aa25db26c045eaf0879da
Bozhidar1804 <bobo1804@abv.bg>
2024-02-23 13:57:30 +0200

    Solved Ex3 from InheritanceEx

---
+++ Advanced - January 2024/C# OOP/Inheritance - Exercise/03PlayersandMonsters/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DarkKnight darkknight = new DarkKnight("Ivan", 31);
+            Console.WriteLine(darkknight.ToString());
+        }
+    }
+}
