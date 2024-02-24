7ef81e91970ec216dd022b0bf2894ad7182054b6
Bozhidar1804 <bobo1804@abv.bg>
2024-02-24 14:51:46 +0200

    Solved Ex4 from InheritanceEx

---
+++ Advanced - January 2024/C# OOP/Inheritance - Exercise/04NeedforSpeed/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        public Car (int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+            DefaultFuelConsumption = 3;
+        }
+    }
+}
---
+++ Advanced - January 2024/C# OOP/Inheritance - Exercise/04NeedforSpeed/SportCar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class SportCar : Car
+    {
+        public SportCar(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+            DefaultFuelConsumption = 10;
+        }
+    }
+}
