37c8dee134284de0ec3f6c59f41e33a801502193
Bozhidar1804 <bobo1804@abv.bg>
2024-03-02 13:54:12 +0200

    Solved Ex1 from Exercises

--- Advanced - January 2024/C# OOP/Interfaces and Abstraction - Exercise/01DefineanInterfaceIPerson/StartUp.cs
+++ Advanced - January 2024/C# OOP/Interfaces and Abstraction - Exercise/01DefineanInterfaceIPerson/StartUp.cs
@@ -1,12 +1,12 @@
 ﻿namespace PersonInfo
 {
-    internal class StartUp
+    public class StartUp
     {
         static void Main(string[] args)
         {
             string name = Console.ReadLine();
             int age = int.Parse(Console.ReadLine());
-            Citizen citizen = new Citizen(name, age);
+            IPerson citizen = new Citizen(name, age);
             Console.WriteLine(citizen.Name);
             Console.WriteLine(citizen.Age);
         }
