fc6a815baae2d6a62e2327ea30b33cabbf7f274a
Bozhidar1804 <bobo1804@abv.bg>
2024-03-08 14:31:56 +0200

    Solved Ex1 from ExceptionsLab

---
+++ Advanced - January 2024/C# OOP/Exceptions and Error Handling - Lab/01SquareRoot/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            try
+            {
+                double parsed = double.Parse(input);
+                if (parsed < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(parsed));
+            } catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            } finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
