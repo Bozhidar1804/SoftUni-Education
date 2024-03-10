a1593412bb56d10f1902649135a25816a75c4c63
Bozhidar1804 <bobo1804@abv.bg>
2024-03-10 14:17:41 +0200

    Solved Ex4 from ExceptionsLab

---
+++ Advanced - January 2024/C# OOP/Exceptions and Error Handling - Lab/04SumofIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _04SumofIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            int sum = 0;
+
+            foreach(string item in input)
+            {
+                try
+                {
+                    int num = int.Parse(item);
+                    sum += num;
+
+                } catch (FormatException fe)
+                {
+                    Console.WriteLine($"The element '{item}' is in wrong format!");
+                } catch (OverflowException oe)
+                {
+                    Console.WriteLine($"The element '{item}' is out of range!");
+                }
+
+                Console.WriteLine($"Element '{item}' processed - current sum: {sum}");
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
