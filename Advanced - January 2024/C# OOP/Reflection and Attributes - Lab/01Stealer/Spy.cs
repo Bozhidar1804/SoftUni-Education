3c94292e58cb961426c6a9d993b2b2b3dc865497
Bozhidar1804 <bobo1804@abv.bg>
2024-03-17 23:20:05 +0200

    Solved Ex1 and Ex2 from ReflAndAttrLab

---
+++ Advanced - January 2024/C# OOP/Reflection and Attributes - Lab/01Stealer/Spy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stealer
+{
+    internal class Spy
+    {
+        public string StealFieldInfo(string nameOfClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(nameOfClass);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            StringBuilder sb = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {nameOfClass}");
+
+            foreach (FieldInfo field in classFields.Where(f => requestedFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
