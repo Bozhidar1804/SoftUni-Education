161a2a4e2771b2dcfdb9b60d095432c60c878753
Bozhidar1804 <bobo1804@abv.bg>
2024-03-19 01:27:57 +0200

    Solved Ex2 from UnitTestingLab

--- Advanced - January 2024/C# OOP/Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
+++ Advanced - January 2024/C# OOP/Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
@@ -1,14 +1,54 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
     [TestFixture]
     public class DummyTests
     {
-        [Test]
-        public void Test1()
-        {
+        private Axe axe;
+        private Dummy dummy;
 
+        [SetUp]
+        public void SetUp()
+        {
+            axe = new Axe(10, 10);
+            dummy = new Dummy(10, 10);
+        }
+
+        [Test]
+        public void DummyLosesHealthCorrectly()
+        {
+            dummy.TakeAttack(2);
+            Assert.AreEqual(dummy.Health, 8);
+        }
+
+        [Test]
+        public void DeadDummyThrowsAnException()
+        {
+            dummy = new Dummy(-10, 10);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                dummy.TakeAttack(1);
+            });
+        }
+
+        [Test]
+        public void DeadDummyGivesXP()
+        {
+            dummy = new Dummy(-10, 10);
+
+            Assert.AreEqual(dummy.GiveExperience(), 10);
+        }
+
+        [Test]
+        public void AliveDummyCantGiveXP()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                dummy.GiveExperience();
+            });
         }
     }
 }
