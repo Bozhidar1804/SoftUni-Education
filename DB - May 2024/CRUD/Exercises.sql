ffa30052b7574800b7ecddc794bf441f6cb0b54a
Bozhidar1804 <bobo1804@abv.bg>
2024-05-15 16:46:54 +0300

    Solved around 10 exercises from CRUD Exercises

--- DB - May 2024/CRUD/Exercises.sql
+++ DB - May 2024/CRUD/Exercises.sql
@@ -34,7 +34,64 @@ SELECT [FirstName], [LastName], [JobTitle]
  WHERE Salary >= 20000 AND Salary <= 30000
 
 -- 10
-SELECT CONCAT([FirstName], ' ', [MiddleName], ' ', [LastName])
+SELECT CONCAT([FirstName], ' ', [MiddleName], ' ', [LastName]) -- SELECT CONCAT_WS(' ', ....)
     AS [Full Name]
   FROM Employees
- WHERE Salary = 25000 OR Salary = 14000 OR Salary = 12500 OR Salary = 23600
+ WHERE Salary IN (25000, 14000, 12500, 23600)
+
+-- 11
+SELECT [FirstName], [LastName]
+  FROM Employees
+ WHERE ManagerID IS NULL
+
+-- 12
+SELECT [FirstName], [LastName], [Salary]
+  FROM Employees
+ WHERE Salary > 50000
+ ORDER BY Salary DESC
+
+-- 13
+SELECT TOP (5) [FirstName], [LastName]
+  FROM Employees
+ORDER BY Salary DESC
+
+-- 14
+SELECT [FirstName], [LastName]
+  FROM Employees
+WHERE DepartmentID <> 4
+
+-- 15
+  SELECT *
+    FROM Employees
+ORDER BY [Salary] DESC,
+         [FirstName],
+		 [LastName] DESC,
+		 [MiddleName]
+
+-- 16
+CREATE VIEW [V_EmployeesSalaries]
+         AS (
+				SELECT [FirstName], [LastName], [Salary]
+				  FROM Employees
+		    )
+
+-- 17
+CREATE VIEW [V_EmployeeNameJobTitle]
+		 AS (
+				SELECT CONCAT([FirstName], ' ', [MiddleName], ' ', [LastName])
+					AS [Full Name],
+					   [JobTitle]
+					AS [Job Title]
+				  FROM Employees 
+			)
+
+-- 18
+SELECT DISTINCT [JobTitle]
+		   FROM Employees
+
+-- 19
+SELECT TOP (10) *
+      FROM Projects
+  ORDER BY [StartDate], [Name]
+
+-- 20
