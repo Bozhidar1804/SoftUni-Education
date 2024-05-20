d0de73a83e38e143fc54eae6813f72b9c260a791
Bozhidar1804 <bobo1804@abv.bg>
2024-05-21 02:30:10 +0300

    Solved Exercises 1 to 3 from Table Relations Exercises

---
+++ DB - May 2024/Table Relations/Exercises 1 to 3.sql
@@ -0,0 +1,102 @@
+-- 1
+CREATE TABLE Passports(
+	PassportId INT PRIMARY KEY IDENTITY(101, 1),
+	PassportNumber NVARCHAR(8) NOT NULL	
+)
+
+CREATE TABLE Persons(
+	PersonID INT PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(50) NOT NULL,
+	Salary DECIMAL(8,2) NOT NULL,
+	PassportID INT,
+	FOREIGN KEY (PassportId) REFERENCES Passports(PassportId)
+)
+
+INSERT INTO Passports (PassportNumber)
+VALUES
+('N34FG21B'),
+('K65LO4R7'),
+('ZE657QP2')
+
+INSERT INTO Persons (FirstName, Salary, PassportID)
+VALUES
+('Roberto', 43300, 102),
+('Tom', 56100, 103),
+('Yana', 60200, 101)
+
+SELECT * FROM Persons
+SELECT * FROM Passports
+
+-- 2
+CREATE TABLE Manufacturers(
+	ManufacturerID INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL,
+	EstablishedOn DATE
+)
+
+CREATE TABLE Models(
+	ModelId INT PRIMARY KEY NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL,
+	ManufacturerID INT,
+	FOREIGN KEY (ManufacturerID) REFERENCES Manufacturers(ManufacturerID)
+)
+
+INSERT INTO Manufacturers ([Name], EstablishedOn)
+VALUES
+('BMW', '07/03/1916'),
+('Tesla', '01/01/2003'),
+('Lada', '01/05/1966')
+
+INSERT INTO Models(ModelId, [Name], ManufacturerID)
+VALUES
+(101, 'X1', 1),
+(102, 'i6', 1),
+(103, 'Model S', 2),
+(104, 'Model X', 2),
+(105, 'Model 3', 2),
+(106, 'Nova', 3)
+
+SELECT * FROM Manufacturers
+SELECT * FROM Models
+
+-- 3
+CREATE TABLE Students(
+	StudentID INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Exams(
+	ExamID INT PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE StudentsExams(
+	StudentID INT,
+	ExamID INT,
+	PRIMARY KEY (StudentID, ExamID)
+)
+
+INSERT INTO Students([Name])
+VALUES
+('Mila'),
+('Toni'),
+('Ron')
+
+INSERT INTO Exams(ExamId, [Name])
+VALUES
+(101, 'SpringMVC'),
+(102, 'Neo4j'),
+(103, 'Oracle 11g')
+
+INSERT INTO StudentsExams(StudentID, ExamID)
+VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103)
+
+SELECT * FROM Students
+SELECT * FROM Exams
+SELECT * FROM StudentsExams
