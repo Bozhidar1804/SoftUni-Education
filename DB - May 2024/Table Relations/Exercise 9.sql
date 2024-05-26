bf8328cc7b8b2ae6b95d1862a85504be1c6ddb3b
Bozhidar1804 <bobo1804@abv.bg>
2024-05-26 15:01:43 +0300

    Solved Ex9 from TableRelationsEx

---
+++ DB - May 2024/Table Relations/Exercise 9.sql
@@ -0,0 +1,16 @@
+--09
+USE [Geography]
+
+GO
+
+SELECT * FROM Peaks
+SELECT * FROM Mountains
+
+   SELECT m.MountainRange, p.PeakName, p.Elevation
+ 	 FROM Peaks
+ 	   AS [p]
+LEFT JOIN Mountains
+	   AS [m]
+	   ON p.MountainId = m.Id
+	WHERE m.MountainRange = 'Rila'
+ ORDER BY p.Elevation DESC
