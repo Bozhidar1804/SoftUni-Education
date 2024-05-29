d2d2bed5ee2b04fa322f79f554009307bbfec7ce
Bozhidar1804 <bobo1804@abv.bg>
2024-05-29 23:49:08 +0300

    Solved Ex1 from SubqueriesEx

---
+++ DB - May 2024/Subqueries and Joins/Exercises.sql
@@ -0,0 +1,12 @@
+-- 01
+SELECT TOP (5)
+	e.EmployeeId,
+	e.JobTitle,
+	e.AddressId,
+	a.AddressText
+FROM Employees AS e
+JOIN Addresses AS a 
+ON e.AddressID = a.AddressID
+ORDER BY AddressID
+
+-- 02
