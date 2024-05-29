7d853ae347f93bd8fe0e28d539d882d697e3fd7a
Bozhidar1804 <bobo1804@abv.bg>
2024-05-29 14:51:55 +0300

    Solved Exercises from FunctionsEx

--- DB - May 2024/Built-in Functions Exercises/Exercises.sql
+++ DB - May 2024/Built-in Functions Exercises/Exercises.sql
@@ -31,3 +31,29 @@ SELECT
 FROM Towns
 WHERE LEN([Name]) IN(5, 6)
 ORDER BY [Name]
+
+-- 06
+SELECT
+	TownID,
+	[Name]
+FROM Towns
+WHERE LEFT([Name], 1) IN ('M', 'K', 'B', 'E')
+ORDER BY [Name]
+
+
+-- 07
+SELECT
+	TownID,
+	[Name]
+FROM Towns
+WHERE LEFT([Name], 1) NOT IN ('R', 'B', 'D')
+ORDER BY [Name]
+
+
+--08
+CREATE VIEW [V_EmployeesHiredAfter2000] AS
+SELECT
+	FirstName,
+	LastName
+FROM Employees
+WHERE YEAR([HireDate]) > 2000
