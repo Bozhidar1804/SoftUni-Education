cca4a683b005b2fa54a922917bcfc0da10020d4d
Bozhidar1804 <bobo1804@abv.bg>
2024-06-01 04:26:28 +0300

    Solved Ex7 from SubqueriesAndJoinsExercises

--- DB - May 2024/Subqueries and Joins/Exercises.sql
+++ DB - May 2024/Subqueries and Joins/Exercises.sql
@@ -53,3 +53,16 @@ FROM Employees AS [e]
 LEFT JOIN EmployeesProjects AS [ep]
 ON e.EmployeeID = ep.EmployeeID
 WHERE ep.ProjectID IS NULL
+
+-- 07
+SELECT TOP (5)
+	e.EmployeeID,
+	e.FirstName,
+	p.[Name]
+FROM Employees AS e
+LEFT JOIN EmployeesProjects AS ep
+ON e.EmployeeID = ep.EmployeeID
+LEFT JOIN Projects AS p
+ON ep.ProjectID = p.ProjectID
+WHERE p.StartDate > '2002-08-13' AND p.EndDate IS NULL
+ORDER BY e.EmployeeID
