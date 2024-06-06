d428640a231d78ff74ea07884f1deb7569eaa965
Bozhidar1804 <bobo1804@abv.bg>
2024-06-06 15:03:12 +0300

    Solved Ex3 from IndicesAndDataAggEx

--- DB - May 2024/Indices and Data Aggregation.sql
+++ DB - May 2024/Indices and Data Aggregation.sql
@@ -9,5 +9,10 @@ SELECT
 FROM WizzardDeposits
 
 -- 03
+SELECT
+	DepositGroup,
+	MAX(MagicWandSize) AS [LongestMagicWand]
+FROM WizzardDeposits
+GROUP BY DepositGroup
 
 SELECT * FROM WizzardDeposits
