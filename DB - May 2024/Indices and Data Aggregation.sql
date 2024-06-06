b15071eeed7a3480f4ee48b1f75bd23044f9c59f
Bozhidar1804 <bobo1804@abv.bg>
2024-06-06 15:08:17 +0300

    Solved Ex4 from IndicesAndDataAggEx

--- DB - May 2024/Indices and Data Aggregation.sql
+++ DB - May 2024/Indices and Data Aggregation.sql
@@ -15,4 +15,12 @@ SELECT
 FROM WizzardDeposits
 GROUP BY DepositGroup
 
+-- 04
+SELECT TOP (2)
+	DepositGroup
+FROM WizzardDeposits
+GROUP BY DepositGroup
+ORDER BY AVG(MagicWandSize)
+
+-- 05
 SELECT * FROM WizzardDeposits
