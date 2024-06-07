ec134f92058a817bb7b583abfcd85b1d087b621a
Bozhidar1804 <bobo1804@abv.bg>
2024-06-07 12:58:58 +0300

    Solved Ex11 from IndicesAndDataAggEx

--- DB - May 2024/Indices and Data Aggregation/Indices and Data Aggregation.sql
+++ DB - May 2024/Indices and Data Aggregation/Indices and Data Aggregation.sql
@@ -91,7 +91,16 @@ GROUP BY LEFT(FirstName, 1)
 ORDER BY FirstLetter
 
 -- 11
+SELECT
+	DepositGroup,
+	IsDepositExpired,
+	AVG(DepositInterest) AS [AverageInterest]
+FROM WizzardDeposits
+WHERE DepositStartDate > '01/01/1985'
+GROUP BY DepositGroup, IsDepositExpired
+ORDER BY DepositGroup DESC, IsDepositExpired
 
 
+-- 12
 
 SELECT * FROM WizzardDeposits
