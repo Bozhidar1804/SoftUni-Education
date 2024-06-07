0abd98eb16d5e1f0c0efb8a066a31671348cb58a
Bozhidar1804 <bobo1804@abv.bg>
2024-06-07 13:14:28 +0300

    Solved Ex11 from IndicesAndDataAGgEx

--- DB - May 2024/Indices and Data Aggregation/Indices and Data Aggregation.sql
+++ DB - May 2024/Indices and Data Aggregation/Indices and Data Aggregation.sql
@@ -102,5 +102,17 @@ ORDER BY DepositGroup DESC, IsDepositExpired
 
 
 -- 12
+SELECT
+	SUM([Difference]) AS [SumDifference]
+FROM (
+		SELECT
+			FirstName AS [Host Wizard],
+			DepositAmount AS [Host Wizard Deposit],
+			LEAD(FirstName) OVER(ORDER BY Id) AS [Guest Wizard],
+			LEAD(DepositAmount) OVER(ORDER BY Id) AS [Guest Wizard Deposit],
+			DepositAmount - LEAD(DepositAmount) OVER(ORDER BY Id) AS [Difference]
+		FROM WizzardDeposits
+) AS [TableSubquery]
+
 
 SELECT * FROM WizzardDeposits
