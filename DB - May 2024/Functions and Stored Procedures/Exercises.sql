ad39f4aece17533efe35a2b33faaf9ddd780d23e
Bozhidar1804 <bobo1804@abv.bg>
2024-06-10 23:55:20 +0300

    Solved Ex1 and Ex2 from FuncAndStoredProcEx

---
+++ DB - May 2024/Functions and Stored Procedures/Exercises.sql
@@ -0,0 +1,27 @@
+-- 01
+CREATE PROCEDURE usp_GetEmployeesSalaryAbove35000
+	AS
+	BEGIN
+		SELECT
+			FirstName,
+			LastName
+		FROM Employees
+		WHERE Salary > 35000
+	END
+
+EXEC usp_GetEmployeesSalaryAbove35000
+
+-- 02
+CREATE OR ALTER PROCEDURE [usp_GetEmployeesSalaryAboveNumber] @GivenValue DECIMAL(18, 4)
+	AS
+	BEGIN
+		SELECT
+			FirstName,
+			LastName
+		FROM Employees
+		WHERE Salary >= @GivenValue
+	END
+
+EXEC usp_GetEmployeesSalaryAboveNumber 48100
+
+-- 03
