76adbec5a69d0427d719df236c64f0b19085deae
Bozhidar1804 <bobo1804@abv.bg>
2024-06-11 00:50:55 +0300

    Solved Ex5 from FuncsAndStoredProcsEx

--- DB - May 2024/Functions and Stored Procedures/Exercises.sql
+++ DB - May 2024/Functions and Stored Procedures/Exercises.sql
@@ -59,3 +59,27 @@ SELECT * FROM Addresses
 SELECT * FROM Towns
 
 -- 05
+CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4))
+RETURNS NVARCHAR(10)
+AS
+BEGIN
+	DECLARE @SalaryLevel NVARCHAR(10)
+
+	IF(@salary < 30000)
+	SET @SalaryLevel = 'Low'
+
+	ELSE IF(@salary BETWEEN 30000 AND 50000)
+	SET @SalaryLevel = 'Average'
+
+	ELSE IF(@salary > 50000)
+	SET @SalaryLevel = 'High'
+
+	RETURN @SalaryLevel
+END
+
+SELECT
+	Salary,
+	dbo.ufn_GetSalaryLevel(Salary) AS [Salary Level]
+FROM Employees
+
+-- 06
