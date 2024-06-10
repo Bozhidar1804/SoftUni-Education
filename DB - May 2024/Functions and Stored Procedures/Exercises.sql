34c98d93bdf381764a09d4de72cd74ff631a2fe8
Bozhidar1804 <bobo1804@abv.bg>
2024-06-11 01:22:14 +0300

    Solved Ex7 from FuncsAndStoredProcsEx

--- DB - May 2024/Functions and Stored Procedures/Exercises.sql
+++ DB - May 2024/Functions and Stored Procedures/Exercises.sql
@@ -96,3 +96,26 @@ CREATE PROCEDURE usp_EmployeesBySalaryLevel @LevelOfSalary VARCHAR(10)
 EXEC usp_EmployeesBySalaryLevel 'High'
 
 -- 07
+CREATE FUNCTION ufn_IsWordComprised(@setOfLetters VARCHAR(50), @word VARCHAR(50))
+RETURNS BIT
+AS
+BEGIN
+	DECLARE @wordIndex INT = 1;
+
+	WHILE (@wordIndex <= LEN(@word))
+	BEGIN
+		DECLARE @currentCharacter CHAR = SUBSTRING(@word, @wordIndex, 1)
+
+		IF (CHARINDEX(@currentCharacter, @setOfLetters) = 0)
+		BEGIN
+			RETURN 0;
+		END
+
+		SET @wordIndex += 1;
+	END
+	RETURN 1;
+END
+
+SELECT dbo.ufn_IsWordComprised('oistmiahf', 'Sofia')
+
+-- 08
