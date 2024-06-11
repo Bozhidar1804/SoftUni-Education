c89674cf85d3041aea1d4e21926265c4fbcef977
Bozhidar1804 <bobo1804@abv.bg>
2024-06-11 18:14:54 +0300

    Solved Ex4 from ExamPrep01

--- DB - May 2024/ExamPrep15Oct2023/Exercises.sql
+++ DB - May 2024/ExamPrep15Oct2023/Exercises.sql
@@ -79,6 +79,24 @@ SET Email = Null
 WHERE [Name] LIKE '%MA%'
 
 -- 04
+DECLARE @TouristsToDelete TABLE(Id INT)
+
+INSERT INTO @TouristsToDelete (Id)
+	SELECT Id
+	FROM Tourists
+	WHERE [Name] LIKE '%Smith'
+
+DELETE FROM Bookings
+WHERE [TouristId] IN (
+						SELECT Id
+						FROM @TouristsToDelete
+					 )
+
+DELETE FROM Tourists
+WHERE Id IN (
+				SELECT Id
+				FROM @TouristsToDelete
+			)
 
 
-SELECT * FROM Tourists
+-- 05
