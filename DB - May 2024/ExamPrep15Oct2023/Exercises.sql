46967534171309805b59c04f0ba86674c13014a4
Bozhidar1804 <bobo1804@abv.bg>
2024-06-11 18:38:54 +0300

    Solved Ex8 from ExamPrep01

--- DB - May 2024/ExamPrep15Oct2023/Exercises.sql
+++ DB - May 2024/ExamPrep15Oct2023/Exercises.sql
@@ -132,3 +132,16 @@ SELECT * FROM Destinations
 
 
 -- 07
+SELECT 
+	t.Id,
+	t.[Name],
+	t.PhoneNumber
+FROM Tourists AS t
+LEFT JOIN Bookings AS b
+ON b.TouristId = t.Id
+WHERE b.Id IS NULL
+ORDER BY t.[Name] ASC
+
+SELECT * FROM Tourists
+
+-- 08
