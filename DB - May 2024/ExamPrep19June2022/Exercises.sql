4ed219ff89ba23ab941cd8d8562bfac21471e826
Bozhidar1804 <bobo1804@abv.bg>
2024-06-11 23:08:35 +0300

    Solved Ex1 and Ex2 from ExamPrep02

---
+++ DB - May 2024/ExamPrep19June2022/Exercises.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE Zoo
+
+-- 01
+CREATE TABLE Owners(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	PhoneNumber VARCHAR(15) NOT NULL,
+	[Address] VARCHAR(50)
+)
+
+CREATE TABLE AnimalTypes(
+	Id INT PRIMARY KEY IDENTITY,
+	AnimalType VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Cages(
+	Id INT PRIMARY KEY IDENTITY,
+	AnimalTypeId INT FOREIGN KEY REFERENCES AnimalTypes(Id) NOT NULL
+)
+
+CREATE TABLE Animals(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(30) NOT NULL,
+	BirthDate DATE NOT NULL,
+	OwnerId INT FOREIGN KEY REFERENCES Owners(Id),
+	AnimalTypeId INT FOREIGN KEY REFERENCES AnimalTypes(Id) NOT NULL
+)
+
+CREATE TABLE AnimalsCages(
+	CageId INT FOREIGN KEY REFERENCES Cages(Id) NOT NULL,
+	AnimalId INT FOREIGN KEY REFERENCES Animals(Id) NOT NULL,
+	PRIMARY KEY (CageId, AnimalId)
+)
+
+CREATE TABLE VolunteersDepartments(
+	Id INT PRIMARY KEY IDENTITY,
+	DepartmentName VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Volunteers(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	PhoneNumber VARCHAR(15) NOT NULL,
+	[Address] VARCHAR(50),
+	AnimalId INT FOREIGN KEY REFERENCES Animals(Id),
+	DepartmentId INT FOREIGN KEY REFERENCES VolunteersDepartments(Id) NOT NULL
+)
+
+-- 02
+INSERT INTO Volunteers([Name], PhoneNumber, [Address], AnimalId, DepartmentId)
+VALUES ('Anita Kostova', '0896365412', 'Sofia, 5 Rosa str.', 15, 1),
+('Dimitur Stoev', '0877564223', null, 42, 4),
+('Kalina Evtimova', '0896321112', 'Silistra, 21 Breza str.', 9, 7),
+('Stoyan Tomov', '0898564100', 'Montana, 1 Bor str.', 18, 8),
+('Boryana Mileva', '0888112233', null, 31, 5)
+
+INSERT INTO Animals([Name], BirthDate, OwnerId, AnimalTypeId)
+VALUES ('Giraffe', '2018-09-21', 21, 1),
+('Harpy Eagle', '2015-04-17', 15, 3),
+('Hamadryas Baboon', '2017-11-02', null, 1),
+('Tuatara', '2021-06-30', 2, 4)
+
+-- 03
