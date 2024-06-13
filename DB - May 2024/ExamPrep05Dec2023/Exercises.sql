1f7e4e8b5e22a1adc8b0d4cf1687f21c514da4f0
Bozhidar1804 <bobo1804@abv.bg>
2024-06-13 14:04:11 +0300

    Solved Ex8 from ExamPrep03

--- DB - May 2024/ExamPrep05Dec2023/Exercises.sql
+++ DB - May 2024/ExamPrep05Dec2023/Exercises.sql
@@ -152,3 +152,21 @@ SELECT * FROM RailwayStations
 SELECT * FROM Towns
 
 -- 08
+SELECT TOP(3)
+	t.Id AS [TrainId],
+	t.HourOfDeparture,
+	ti.Price AS [TicketPrice],
+	[to].[Name] AS [Destination]
+FROM Trains AS t
+JOIN Tickets as ti
+ON t.Id = ti.TrainId
+JOIN Towns AS [to]
+ON t.ArrivalTownId = [to].Id
+WHERE CAST(t.HourOfDeparture AS DATETIME) BETWEEN '08:00' AND '08:59' AND ti.Price > 50
+ORDER BY ti.Price
+
+SELECT * FROM Trains
+SELECT * FROM Tickets
+SELECT * FROM Towns
+
+-- 09
