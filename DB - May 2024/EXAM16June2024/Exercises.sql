25189ac3949f352356eea240b9c82a25f373f8b0
Bozhidar1804 <bobo1804@abv.bg>
2024-06-16 10:18:45 +0300

    Solved Ex8 from the Exam

--- DB - May 2024/EXAM16June2024/Exercises.sql
+++ DB - May 2024/EXAM16June2024/Exercises.sql
@@ -172,4 +172,14 @@ SELECT * FROM Contacts
 SELECT * FROM Books
 
 -- 08
+SELECT TOP(3)
+	b.Title,
+	b.YearPublished AS [Year],
+	g.[Name]
+FROM Books AS b
+JOIN Genres AS g ON b.GenreId = g.Id
+WHERE (b.YearPublished > 2000 AND b.Title LIKE '%a%') OR (b.YearPublished < 1950 AND g.[Name] LIKE '%Fantasy%')
+ORDER BY b.Title, b.YearPublished DESC
 
+
+-- 09
