def2bf42a8ff26f62e883c5fe014ab57b73e9f9c
Bozhidar1804 <bobo1804@abv.bg>
2024-06-30 20:59:17 +0300

    Established my first connection between SQL Server and VS

---
+++ Entity Framework - June 2024/EF Introduction/DbFirstDemo/Data/SoftUniDbContect.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using DbFirstDemo.Data.Models;
+
+namespace DbFirstDemo.Data
+{
+    public partial class SoftUniDbContect : DbContext
+    {
+        public SoftUniDbContect()
+        {
+        }
+
+        public SoftUniDbContect(DbContextOptions<SoftUniDbContect> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Address> Addresses { get; set; } = null!;
+        public virtual DbSet<Department> Departments { get; set; } = null!;
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<NewTable> NewTables { get; set; } = null!;
+        public virtual DbSet<Project> Projects { get; set; } = null!;
+        public virtual DbSet<Town> Towns { get; set; } = null!;
+        public virtual DbSet<VEmployeesSalary> VEmployeesSalaries { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-FI5CDKM\\SQLEXPRESS;Database=SoftUni;Integrated Security=True;TrustServerCertificate=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.HasOne(d => d.Town)
+                    .WithMany(p => p.Addresses)
+                    .HasForeignKey(d => d.TownId)
+                    .HasConstraintName("FK_Addresses_Towns");
+            });
+
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.HasOne(d => d.Manager)
+                    .WithMany(p => p.Departments)
+                    .HasForeignKey(d => d.ManagerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Departments_Employees");
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasOne(d => d.Address)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.AddressId)
+                    .HasConstraintName("FK_Employees_Addresses");
+
+                entity.HasOne(d => d.Department)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.DepartmentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Employees_Departments");
+
+                entity.HasOne(d => d.Manager)
+                    .WithMany(p => p.InverseManager)
+                    .HasForeignKey(d => d.ManagerId)
+                    .HasConstraintName("FK_Employees_Employees");
+
+                entity.HasMany(d => d.Projects)
+                    .WithMany(p => p.Employees)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "EmployeesProject",
+                        l => l.HasOne<Project>().WithMany().HasForeignKey("ProjectId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_EmployeesProjects_Projects"),
+                        r => r.HasOne<Employee>().WithMany().HasForeignKey("EmployeeId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_EmployeesProjects_Employees"),
+                        j =>
+                        {
+                            j.HasKey("EmployeeId", "ProjectId");
+
+                            j.ToTable("EmployeesProjects");
+
+                            j.IndexerProperty<int>("EmployeeId").HasColumnName("EmployeeID");
+
+                            j.IndexerProperty<int>("ProjectId").HasColumnName("ProjectID");
+                        });
+            });
+
+            modelBuilder.Entity<NewTable>(entity =>
+            {
+                entity.Property(e => e.EmployeeId).ValueGeneratedOnAdd();
+            });
+
+            modelBuilder.Entity<VEmployeesSalary>(entity =>
+            {
+                entity.ToView("V_EmployeesSalaries");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
