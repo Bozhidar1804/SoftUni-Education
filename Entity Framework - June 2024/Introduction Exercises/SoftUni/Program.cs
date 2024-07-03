b5a3ce472d6b67065d4a9c467d6d7f8444812989
Bozhidar1804 <bobo1804@abv.bg>
2024-07-03 04:54:27 +0300

    Solved Ex2 from EFIntroductionEx

---
+++ Entity Framework - June 2024/Introduction Exercises/SoftUni/Program.cs
@@ -0,0 +1,13 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            Console.WriteLine("Connection successful!");
+        }
+    }
+}
