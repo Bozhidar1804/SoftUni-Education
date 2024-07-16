cee3dd3e3441428befcd1060c8a826f1eedb1049
Bozhidar1804 <bobo1804@abv.bg>
2024-07-16 15:34:01 +0300

    Solved Ex2 from AdvancedQueryingEx

---
+++ Entity Framework - June 2024/AdvancedQuerying Exercises/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=DESKTOP-FI5CDKM\\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
---
+++ Entity Framework - June 2024/AdvancedQuerying Exercises/BookShop/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var dbContext = new BookShopContext();
+            DbInitializer.ResetDatabase(dbContext);
+
+            string ageRestriction = Console.ReadLine();
+            Console.WriteLine(GetBooksByAgeRestriction(dbContext, ageRestriction));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            try
+            {
+                AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+                string[] bookTitles = context.Books
+                    .Where(b => b.AgeRestriction == ageRestriction)
+                    .OrderBy(b => b.Title)
+                    .Select(b => b.Title)
+                    .ToArray();
+
+                return String.Join(Environment.NewLine, bookTitles);
+            }  catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
+
+
