fbbf0d25ad959e10fe5130bf1845366f9f4e0729
Bozhidar1804 <bobo1804@abv.bg>
2024-07-24 14:11:37 +0300

    Solved Ex9 from XML Ex

---
+++ Entity Framework - June 2024/XML Exercises/CarDealer/DTOs/Import/ImportSupplierDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+{
+    [XmlType("Supplier")]
+    public class ImportSupplierDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+        [XmlElement("isImporter")]
+        public bool IsImporter { get; set; }
+    }
+}
--- Entity Framework - June 2024/XML Exercises/CarDealer/StartUp.cs
+++ Entity Framework - June 2024/XML Exercises/CarDealer/StartUp.cs
@@ -1,10 +1,42 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using System.Xml.Serialization;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            CarDealerContext context = new CarDealerContext();
+            string suppliers = File.ReadAllText("../../../Datasets/suppliers.xml");
+            Console.WriteLine(ImportSuppliers(context, suppliers));
+        }
 
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportSupplierDto[]),
+                    new XmlRootAttribute("Suppliers"));
+
+            ImportSupplierDto[] importDtos;
+
+            using(var reader = new StringReader(inputXml))
+            {
+                importDtos = (ImportSupplierDto[])xmlSerializer.Deserialize(reader);
+            };
+
+            Supplier[] suppliers = importDtos
+                .Select(dto => new Supplier()
+                {
+                    Name = dto.Name,
+                    IsImporter = dto.IsImporter
+                }).ToArray();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
         }
     }
 }
