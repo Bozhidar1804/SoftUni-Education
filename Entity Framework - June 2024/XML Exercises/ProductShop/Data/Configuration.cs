66b2b1629af0966e7d9b2b84971da50e747d0627
Bozhidar1804 <bobo1804@abv.bg>
2024-07-26 04:05:19 +0300

    Solved Ex1 from XML Ex

--- Entity Framework - June 2024/XML Exercises/ProductShop/Data/Configuration.cs
+++ Entity Framework - June 2024/XML Exercises/ProductShop/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public const string ConnectionString = @"Server=.;Database=ProductShop;Integrated Security=True;Encrypt=False";
+        public const string ConnectionString = @"Server=DESKTOP-FI5CDKM\SQLEXPRESS;Database=ProductShop;Integrated Security=True;Encrypt=False";
     }
 }
--- Entity Framework - June 2024/XML Exercises/ProductShop/StartUp.cs
+++ Entity Framework - June 2024/XML Exercises/ProductShop/StartUp.cs
@@ -1,10 +1,44 @@
-﻿namespace ProductShop
+﻿using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using System.Xml.Serialization;
+
+namespace ProductShop
 {
     public class StartUp
     {
         public static void Main()
         {
+            ProductShopContext context = new ProductShopContext();
+            string users = File.ReadAllText("../../../Datasets/users.xml");
+            Console.WriteLine(ImportUsers(context, users));
+        }
 
+        // Problem 01
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(UserImportDto[]),
+                    new XmlRootAttribute("Users"));
+
+            UserImportDto[] usersDtos;
+
+            using (var reader = new StringReader(inputXml))
+            {
+                usersDtos = (UserImportDto[])xmlSerializer.Deserialize(reader);
+            }
+
+            User[] users = usersDtos
+                .Select(u => new User()
+                {
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Age = u.Age
+                }).ToArray();
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
         }
     }
 }
