d136a0c02195d6f6e61fe5eefbab26a3d7e04b8f
Bozhidar1804 <bobo1804@abv.bg>
2024-09-16 00:23:18 +0300

    Started exercising ASP.NET MVC

---
+++ Web - Sept - Dec 2024/ASP.NET Fundamentals/IntroductionExercises/Practice/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Practice.Models;
+using System.Diagnostics;
+
+namespace Practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello World!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
