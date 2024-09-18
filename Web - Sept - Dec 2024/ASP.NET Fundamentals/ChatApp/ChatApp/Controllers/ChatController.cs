f2096fa3d86d7fdc044b06190dc88f29b0d458ca
Bozhidar1804 <bobo1804@abv.bg>
2024-09-18 03:22:32 +0300

    Initialized ChatApp

---
+++ Web - Sept - Dec 2024/ASP.NET Fundamentals/ChatApp/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
