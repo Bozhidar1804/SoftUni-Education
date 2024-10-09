c61cbd5cc446566fc8554ce8f6293dcc76913bb5
Bozhidar1804 <bobo1804@abv.bg>
2024-10-09 20:32:10 +0300

    Practicing Asynchronous Processing

---
+++ Web - Sept - Dec 2024/ASP.NET Fundamentals/AsynchronousProcessingExercises/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace AsynchronousProcessingExercises
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EvenNumbersThread();
+        }
+
+        private static void EvenNumbersThread()
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            Thread evens = new Thread(() => PrintEvenNumbers(start, end));
+            evens.Start();
+            evens.Join();
+            Console.WriteLine("Thread finished work.");
+        }
+
+        private static void PrintEvenNumbers(int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
