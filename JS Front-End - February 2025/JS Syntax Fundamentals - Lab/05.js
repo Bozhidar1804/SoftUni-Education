ab52c8e12d2ba1d5c7feea4d89eb68df4cab69cd
Bozhidar1804 <bobo1804@abv.bg>
2025-02-20 16:40:11 +0200

    Solved Ex5 from JSFundLab

---
+++ JS Front-End - February 2025/JS Syntax Fundamentals - Lab/05.js
@@ -0,0 +1,22 @@
+function print(num1, num2, operation) {
+  switch (operation) {
+    case ("+"):
+      console.log(num1 + num2);
+      break;
+    case ("-"):
+      console.log(num1 - num2);
+      break;
+    case ("*"):
+      console.log(num1 * num2);
+      break;
+    case ("/"):
+      console.log(num1 / num2);
+      break;
+    case ("%"):
+      console.log(num1 % num2);
+      break;
+    case ("**"):
+      console.log(num1 ** num2);
+      break;
+  }
+}
