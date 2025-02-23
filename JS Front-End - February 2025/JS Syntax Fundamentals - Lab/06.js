fcf88038214ede3856f1377d07ba55f02feeadea
Bozhidar1804 <bobo1804@abv.bg>
2025-02-23 23:08:17 +0200

    Solved Ex6 from JSSyntaxFundLab

---
+++ JS Front-End - February 2025/JS Syntax Fundamentals - Lab/06.js
@@ -0,0 +1,12 @@
+function largestNumber(num1, num2, num3) {
+  let largestNumber;
+  if (num1 > num2 && num1 > num3) {
+    largestNumber = num1
+  } else if (num2 > num1 && num2 > num3) {
+    largestNumber = num2
+  } else if (num3 > num1 && num3 > num2) {
+    largestNumber = num3
+  }
+
+  console.log(`The largest number is ${largestNumber}`);
+}
