eae049896f30d1b0d505058ef22df434725708de
Bozhidar1804 <bobo1804@abv.bg>
2025-04-06 23:56:43 +0300

    Solved Ex5 from DOMIntroEx

--- JS Front-End - February 2025/DOM Introduction - Exercise/05. Table - Search Engine/table-search-engine.js
+++ JS Front-End - February 2025/DOM Introduction - Exercise/05. Table - Search Engine/table-search-engine.js
@@ -1,3 +1,25 @@
 function solve() {
-   //TODO
+   let rows = Array.from(document.querySelectorAll(".container tbody tr"));
+   let searchText = document.getElementById("searchField").value.toLowerCase();
+
+   for (let row of rows) {
+      row.classList.remove("select");
+   }
+
+   if (searchText.trim() === "") {
+      return;
+   }
+
+   for (let row of rows) {
+      let cells = row.querySelectorAll("td");
+
+      for (let cell of cells) {
+         if (cell.textContent.toLowerCase().includes(searchText)) {
+            row.classList.add("select");
+            break;
+         }
+      }
+   }
+
+   document.getElementById("searchField").value = "";
 }
